7f37e1b58cd7aa75835544094785762d0a1963f5
scott <scott@macbook.local>
2014-05-04 00:05:27 -0700

    Adding second function to break_me.py: cause_type_error

--- students/ScottCierski/break_me.py
+++ students/ScottCierski/break_me.py
@@ -4,6 +4,10 @@
 def cause_name_error():
     print(x)
 
+ # This function will cause a TypeError because it attempts to add two different data types (int and string)
+def cause_type_error():
+    print 1 + 'I am a string'
+
 # Useful for running a single function from the command line
 if __name__ == '__main__':
-    cause_name_error()
+    cause_type_error()
