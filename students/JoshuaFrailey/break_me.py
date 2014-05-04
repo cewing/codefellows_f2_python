60278fb0139e8f0330cf0b5ea52bf4563615e7aa
jefrailey <jcaetof@gmail.com>
2014-05-04 12:49:35 -0700

    Added causeTypeError() to break_me.py

--- students/JoshuaFrailey/break_me.py
+++ students/JoshuaFrailey/break_me.py
@@ -3,3 +3,11 @@ def causeNameError():
     Causes a NameError
     '''
     return moose
+
+def causeTypeError():
+    '''
+    Causes a TypeError
+    '''
+    notAString = 45315
+    for letter in notAString:
+        print letter
