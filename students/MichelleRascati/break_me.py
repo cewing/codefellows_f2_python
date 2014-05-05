0b7b4e1bd9f122e94b4d08c157ba5a54ba17e233
miracode <rascatim@gmail.com>
2014-05-05 14:09:09 -0700

    break_contents

---
+++ students/MichelleRascati/break_me.py
@@ -0,0 +1,13 @@
+def fun_name():
+    x = y+1
+
+def fun_type():
+    a = 'hello' + 1
+
+def fun_syntax():
+    fo i in range(5):
+        print i
+
+def fun_attribute():
+    string = 'hello'
+    print string.strlwr()
