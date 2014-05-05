7c6e850680195ef8e84558ac9a74c890a19dea25
miracode <rascatim@gmail.com>
2014-05-05 14:23:16 -0700

    break_w_notes

--- students/MichelleRascati/break_me.py
+++ students/MichelleRascati/break_me.py
@@ -1,13 +1,17 @@
 def fun_name():
+    #This should cause a NameError
     x = y+1
 
 def fun_type():
+    #This should cause a TypeError
     a = 'hello' + 1
 
 def fun_syntax():
+    #This should cause a SyntaxError
     fo i in range(5):
         print i
 
 def fun_attribute():
+    #This should cause an AttributeError
     string = 'hello'
     print string.strlwr()
