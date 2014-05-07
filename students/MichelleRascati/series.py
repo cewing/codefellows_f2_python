22c2fe057e7568729366ceba4aa1494d606cf774
miracode <rascatim@gmail.com>
2014-05-06 22:38:41 -0700

    Added Lucas function

--- students/MichelleRascati/series.py
+++ students/MichelleRascati/series.py
@@ -1,9 +1,22 @@
 def fibonacci(n):
-    """Return nth number in fibonacci sequence"""
+    """Return nth number in Fibonacci sequence"""
     # Initialize sequence
     seq = [0, 1]
     if n == 1:
         return 0
+    elif n == 2:
+        return 1
+    for i in range(1, n - 1):
+        seq.append(seq[i] + seq[i - 1])
+    return seq[-1]
+
+
+def lucas(n):
+    """Return nth number in Lucas numbers"""
+    # Initialize sequence
+    seq = [2, 1]
+    if n == 1:
+        return 2
     elif n == 2:
         return 1
     for i in range(1, n - 1):
