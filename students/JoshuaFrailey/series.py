c4850330cf38feded24188f486f63c186bc330df
jefrailey <jcaetof@gmail.com>
2014-05-06 19:28:23 -0700

    Added first draft of lucas()

--- students/JoshuaFrailey/series.py
+++ students/JoshuaFrailey/series.py
@@ -1,10 +1,11 @@
 def fibonacci(n):
-    """Return the nth value in the fibonacci series.
+    """Return the nth value in the Fibonacci series.
 
     Arguments:
     n: A nonnegative, nonzero integer.
     Return value: A positive integer.
     """
+
     def check_value(n):
         if not isinstance(n, int):
             raise ValueError
@@ -23,6 +24,33 @@ def fibonacci(n):
     if check_value(n):
         return recursive_fib(n)
 
+
+def lucas(n):
+    """Return the nth value in the Lucas series.
+
+    Arguments:
+    n: A nonnegative, nonzero integer.
+    Return value: A positive integer.
+    """
+
+    def check_value(n):
+        if not isinstance(n, int):
+            raise ValueError
+        if n < 0:
+            raise ValueError
+        return True
+
+    def recursive_lucas(n):
+            if n == 0:
+                return 2
+            elif n == 1:
+                return 1
+            else:
+                return recursive_lucas(n-1) + recursive_lucas(n-2)
+
+    if check_value(n):
+        return recursive_lucas(n)
+
 if __name__ == "__main__":
     fibonacci_vals = [
         (0, 0),
@@ -39,6 +67,13 @@ if __name__ == "__main__":
         (11, 89),
         (12, 144)
         ]
+    lucas_vals = [
+        (0, 2),
+        (1, 1),
+        (2, 3),
+        (3, 4),
+        (4, 7),
+        (4, 11)]
 
     for input_, output in fibonacci_vals:
         assert fibonacci(input_) == output
