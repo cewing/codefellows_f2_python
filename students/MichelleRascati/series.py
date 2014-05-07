3df16f2c95ff0a27cb1bc206366f312a6559e664
miracode <rascatim@gmail.com>
2014-05-06 22:34:57 -0700

    Corrected seq.append in loop

--- students/MichelleRascati/series.py
+++ students/MichelleRascati/series.py
@@ -7,5 +7,5 @@ def fibonacci(n):
     elif n == 2:
         return 1
     for i in range(1, n - 1):
-        seq = seq.append(seq[i] + seq[i - 1])
+        seq.append(seq[i] + seq[i - 1])
     return seq[-1]
