2a06b32e261bcdc71645fb3cdb466a4dd9c9b98e
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-07 11:21:53 -0700

    Added a test block to validate the code. Everything appears to work perfectly.

--- students/NathanHubbell/series.py
+++ students/NathanHubbell/series.py
@@ -17,7 +17,7 @@ def fibonaccci(n):
             thirdInt=0
         return secondInt
 
-print fibonaccci(0)
+
 
 def lucas(n):
     """Take an int 'n', return the 'nth' value in the lucas series"""
@@ -38,7 +38,7 @@ def lucas(n):
             thirdInt=0
         return secondInt
 
-print lucas(0)
+
 
 def sum_series(n,indexZero=0,indexOne=1):
     """Take an int 'n', and the first two values in a series, then return the 'nth' value in the sum series"""
@@ -59,4 +59,39 @@ def sum_series(n,indexZero=0,indexOne=1):
             thirdInt=0
         return secondInt
 
-print sum_series(7)
+if __name__=='__main__':
+    #Test the fibonacci function
+    assert fibonaccci(0)==0
+    assert fibonaccci(1)==1
+    assert fibonaccci(2)==1
+    assert fibonaccci(3)==2
+    assert fibonaccci(4)==3
+    assert fibonaccci(5)==5
+    assert fibonaccci(6)==8
+    #test the lucas function
+    assert lucas(0)==2
+    assert lucas(1)==1
+    assert lucas(2)==3
+    assert lucas(3)==4
+    assert lucas(4)==7
+    assert lucas(5)==11
+    assert lucas(6)==18
+
+    #Test the  fibonacci sum_series function
+    assert sum_series(0)==0
+    assert sum_series(1)==1
+    assert sum_series(2)==1
+    assert sum_series(3)==2
+    assert sum_series(4)==3
+    assert sum_series(5)==5
+    assert sum_series(6)==8
+    #test the lucas sum_series function
+    assert sum_series(0,2,1)==2
+    assert sum_series(1,2,1)==1
+    assert sum_series(2,2,1)==3
+    assert sum_series(3,2,1)==4
+    assert sum_series(4,2,1)==7
+    assert sum_series(5,2,1)==11
+    assert sum_series(6,2,1)==18
+    #Succes!
+    print "All Tests Passed"
