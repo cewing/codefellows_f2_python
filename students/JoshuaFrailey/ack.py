8fb312e0a91d813895f325fb781a10a401ab94d6
jefrailey <jcaetof@gmail.com>
2014-05-06 18:39:14 -0700

    ack.py tested and finalized

--- students/JoshuaFrailey/ack.py
+++ students/JoshuaFrailey/ack.py
@@ -1,10 +1,15 @@
 def ack(m, n):
     """Return the Ackermann-Peter function.
 
-
-    m: a nonnegative integer between 0 and 3 inclusive.
-    n: a nonnegative integer between 0 and 4 inclusive.
+    Arguments:
+    m: A nonnegative integer between 0 and 3 inclusive.
+    n: A nonnegative integer between 0 and 4 inclusive.
+    Return value: A nonnegative integer
     """
+    if (not isinstance(m, int)) or (not isinstance(n, int)):
+        raise ValueError
+    if (m < 0) or (n < 0):
+        raise ValueError
     if m == 0:
         return n+1
     elif n == 0:
@@ -35,3 +40,34 @@ if __name__ == '__main__':
         (3, 3): 61,
         (3, 4): 125,
         }
+
+    for input_ in vals:
+        assert ack(input_[0], input_[1]) == vals[input_]
+
+    badvals = [
+        (-1, -1),
+        (-1, -3.14),
+        (-1, 2.78),
+        (-1, 'a'),
+        (-1, []),
+        ('a', -1),
+        ('a', -3.14),
+        ('a', 2.78),
+        ('a', 'a'),
+        ('a', []),
+        (2.78, -1),
+        (2.78, -3.14),
+        (2.78, 2.78),
+        (2.78, 'a'),
+        (2.78, [])
+        ]
+
+    for badval in badvals:
+        try:
+            result = ack(badval[0], badval[1])
+        except ValueError:
+            pass
+        else:
+            print u"A bad value did not trigger a ValueError"
+
+    print u"All tests passed!"
