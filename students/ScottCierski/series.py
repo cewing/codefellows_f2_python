d0a5e547d791d4741360c475f29ea9eed2757504
scott <scott@macbook.local>
2014-05-06 20:10:28 -0700

    Still failing. 3-1 somehow becomes None type

--- students/ScottCierski/series.py
+++ students/ScottCierski/series.py
@@ -6,16 +6,15 @@ def fibonacci(n):
     # Convert input to int, in case a negative integer is passed in
     n = int(n)
     
-    # Early out if negative integer is passed in 
-    if n < 0:
-        print('Only positive values are allowed.')
-        return None
     # First two value of sequence are fixed at 0 and 1, so just return them if they're passed in
-    elif n < 2:
-        return n
+    if n ==0:
+        return 0
+    elif n ==1:
+        return 1
     else:
-        ret = fibonacci(n-2) + fibonacci(n-1)
-        print ret
+        print fibonacci(n-2), type(fibonacci(n-2))
+        print fibonacci(n-1), type(fibonacci(n-1))
+        # fibonacci(n-2) + fibonacci(n-1)
 
 if __name__ == '__main__':
     n = sys.argv[1]
