bfd0dbda50bb750ce721565672216e7b3a669af6
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-07 11:29:59 -0700

    Final code clean up. Added a few more tests to the code validation.

--- students/NathanHubbell/series.py
+++ students/NathanHubbell/series.py
@@ -2,67 +2,61 @@ def fibonaccci(n):
     """Take an int 'n', return the 'nth' value in the fibonaccci series"""
     firstInt = 0
     secondInt = 1
-
-    if n<0:
+    if n < 0:
         return None
-    elif int(n)!=float(n):
+    elif int(n) != float(n):
         return None
-    elif n==0:
+    elif n == 0:
         return firstInt
     else:
         for i in range(1,n):
             thirdInt = firstInt+secondInt
-            firstInt=secondInt
-            secondInt=thirdInt
-            thirdInt=0
+            firstInt = secondInt
+            secondInt = thirdInt
+            thirdInt = 0
         return secondInt
-
 
 
 def lucas(n):
     """Take an int 'n', return the 'nth' value in the lucas series"""
     firstInt = 2
     secondInt = 1
-
-    if n<0:
+    if n < 0:
         return None
-    elif int(n)!=float(n):
+    elif int(n) != float(n):
         return None
-    elif n==0:
+    elif n == 0:
         return firstInt
     else:
         for i in range(1,n):
             thirdInt = firstInt+secondInt
-            firstInt=secondInt
-            secondInt=thirdInt
-            thirdInt=0
+            firstInt = secondInt
+            secondInt = thirdInt
+            thirdInt = 0
         return secondInt
 
 
-
-def sum_series(n,indexZero=0,indexOne=1):
+def sum_series(n,indexZero = 0,indexOne = 1):
     """Take an int 'n', and the first two values in a series, then return the 'nth' value in the sum series"""
-    firstInt = indexZero
-    secondInt = indexOne
-
-    if n<0:
+    if n < 0:
         return None
-    elif int(n)!=float(n):
+    elif int(n) != float(n):
         return None
-    elif n==0:
-        return firstInt
+    elif n == 0:
+        return indexZero
     else:
         for i in range(1,n):
-            thirdInt = firstInt+secondInt
-            firstInt=secondInt
-            secondInt=thirdInt
-            thirdInt=0
-        return secondInt
+            thirdInt = indexZero+indexOne
+            indexZero = indexOne
+            indexOne = thirdInt
+            thirdInt = 0
+        return indexOne
+
 
 if __name__=='__main__':
     #Test the fibonacci function
     assert fibonaccci(0)==0
-    assert fibonaccci(1)==1
+    assert fibonaccci(1.1)==None
     assert fibonaccci(2)==1
     assert fibonaccci(3)==2
     assert fibonaccci(4)==3
@@ -70,7 +64,7 @@ if __name__=='__main__':
     assert fibonaccci(6)==8
     #test the lucas function
     assert lucas(0)==2
-    assert lucas(1)==1
+    assert lucas(1.1)==None
     assert lucas(2)==3
     assert lucas(3)==4
     assert lucas(4)==7
@@ -78,16 +72,18 @@ if __name__=='__main__':
     assert lucas(6)==18
 
     #Test the  fibonacci sum_series function
+    assert sum_series(-10)==None
     assert sum_series(0)==0
-    assert sum_series(1)==1
+    assert sum_series(1.1)==None
     assert sum_series(2)==1
     assert sum_series(3)==2
     assert sum_series(4)==3
     assert sum_series(5)==5
     assert sum_series(6)==8
     #test the lucas sum_series function
+    assert sum_series(-20,2,1)==None
     assert sum_series(0,2,1)==2
-    assert sum_series(1,2,1)==1
+    assert sum_series(1.1,2,1)==None
     assert sum_series(2,2,1)==3
     assert sum_series(3,2,1)==4
     assert sum_series(4,2,1)==7
