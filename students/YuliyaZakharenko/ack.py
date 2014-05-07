f3fb91b07f37e3fb896a76ba2d30d1e328ae1411
Yuliya Zakharenko <rockroada@gmail.com>
2014-05-06 23:36:19 -0700

    committing attempts to make the test work with assert

--- students/YuliyaZakharenko/ack.py
+++ students/YuliyaZakharenko/ack.py
@@ -6,26 +6,28 @@ def A(m,n):
         return A(m-1, 1)
     else:
         return A(m-1, A(m,n-1))  
+def test_fun():
+    assert A(0,0) == 1
+    assert A(1,0) == 2
+    assert A(2,0) == 3
+    assert A(3,0) == 5
+    assert A(0,1) == 2
+    assert A(1,1) == 3
+    assert A(2,1) == 5
+    assert A(3,1) == 13
+    assert A(0,2) == 3
+    assert A(1,2) == 4
+    assert A(2,2) == 7
+    assert A(3,2) == 29
+    assert A(0,3) == 4
+    assert A(1,3) == 5
+    assert A(2,3) == 9
+    assert A(3,3) == 61
+    assert A(0,4) == 5
+    assert A(1,4) == 6
+    assert A(2,4) == 11
+    assert A(3,4) == 125
+    return "All Tests Pass"    
 
-    if __name__ == "__main__":
-     	assert A(0,0) == 1
-     	assert A(1,0) == 2
-     	assert A(2,0) == 3
-     	assert A(3,0) == 5
-     	assert A(0,1) == 2
-     	assert A(1,1) == 3
-     	assert A(2,1) == 5
-     	assert A(3,1) == 13
-        assert A(0,2) == 3
-     	assert A(1,2) == 4
-     	assert A(2,2) == 7
-     	assert A(3,2) == 29
-     	assert A(0,3) == 4
-     	assert A(1,3) == 5
-     	assert A(2,3) == 9
-     	assert A(3,3) == 61
-     	assert A(0,4) == 5
-     	assert A(1,4) == 6
-     	assert A(2,4) == 11
-     	assert A(3,4) == 125
-     	print "All Tests Pass"
+if __name__ == "__main__":
+    test_fun()
