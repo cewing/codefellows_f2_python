a482f2db21d8e39c3475fc055cbfee031ae80e20
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-07 10:56:46 -0700

    Added a final sum_series function that can mimic the fibonacci, lucas, or any other sum type series.

--- students/NathanHubbell/series.py
+++ students/NathanHubbell/series.py
@@ -39,3 +39,24 @@ def lucas(n):
         return secondInt
 
 print lucas(0)
+
+def sum_series(n,indexZero=0,indexOne=1):
+    """Take an int 'n', and the first two values in a series, then return the 'nth' value in the sum series"""
+    firstInt = indexZero
+    secondInt = indexOne
+
+    if n<0:
+        return None
+    elif int(n)!=float(n):
+        return None
+    elif n==0:
+        return firstInt
+    else:
+        for i in range(1,n):
+            thirdInt = firstInt+secondInt
+            firstInt=secondInt
+            secondInt=thirdInt
+            thirdInt=0
+        return secondInt
+
+print sum_series(7)
