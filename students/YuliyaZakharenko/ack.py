083cf2fd739f53c0c3083007ef3d0ce8fb85da89
Yuliya Zakharenko <rockroada@gmail.com>
2014-05-06 23:22:40 -0700

    adding docstring and assertion block to Ackermann function. Doesn't return All Tests Pass

--- students/YuliyaZakharenko/ack.py
+++ students/YuliyaZakharenko/ack.py
@@ -1,7 +1,31 @@
 def A(m,n):
-    if m ==0:
+    """Return a value based on the Ackermann function"""
+    if m == 0:
         return n+1
     elif m>0 and n ==0:
         return A(m-1, 1)
     else:
-        return A(m-1, A(m,n-1))    
+        return A(m-1, A(m,n-1))  
+
+    if __name__ == "__main__":
+     	assert A(0,0) == 1
+     	assert A(1,0) == 2
+     	assert A(2,0) == 3
+     	assert A(3,0) == 5
+     	assert A(0,1) == 2
+     	assert A(1,1) == 3
+     	assert A(2,1) == 5
+     	assert A(3,1) == 13
+        assert A(0,2) == 3
+     	assert A(1,2) == 4
+     	assert A(2,2) == 7
+     	assert A(3,2) == 29
+     	assert A(0,3) == 4
+     	assert A(1,3) == 5
+     	assert A(2,3) == 9
+     	assert A(3,3) == 61
+     	assert A(0,4) == 5
+     	assert A(1,4) == 6
+     	assert A(2,4) == 11
+     	assert A(3,4) == 125
+     	print "All Tests Pass"
