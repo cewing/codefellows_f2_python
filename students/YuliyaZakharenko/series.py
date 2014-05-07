15f79979fa2085ef31e4e1381c84087184346952
Yuliya Zakharenko <rockroada@gmail.com>
2014-05-07 01:04:16 -0700

    committing lucas function

--- students/YuliyaZakharenko/series.py
+++ students/YuliyaZakharenko/series.py
@@ -1,7 +1,20 @@
 def fibonacci(n):
 	"""return nth value in fibonacci series"""
-	series = [0,1]
-	for i in range(2, n):
-	    series.append(series[i-1] + series[i-2])
-	    #i = i + 1
-        return series[n-1]
+	if n <= 0:
+		print "no value"
+	else:
+	    series = [0,1]
+	    for i in range(2, n):
+	        series.append(series[i-1] + series[i-2])
+            return series[n-1]
+
+
+def lucas(n):
+	"""return nth value in lucas series"""
+	if n <= 0:
+		print "no value"
+	else:
+	    series = [2,1]
+	    for i in range(2, n):
+	        series.append(series[i-1] + series[i-2])
+            return series[n-1]        
