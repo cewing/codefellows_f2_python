2070ebe4cbf459bd7d65797738880c4c24cab5a3
Brent Nunn <brentnunn@yahoo.com>
2014-05-07 15:34:17 -0700

    Switched to a loop to run the tests of the Ackermann function

--- students/BrentNunn/ack.py
+++ students/BrentNunn/ack.py
@@ -3,8 +3,8 @@
 def ack(m, n):
     """Return the Ackermann function for m, n."""
 
-    assert m >= 0
-    assert n >= 0
+    if m < 0 or n < 0:
+        return None
 
     if m == 0:
         return n + 1
@@ -17,32 +17,34 @@ def ack(m, n):
 if __name__ == '__main__':
     print "Testing Ackermann function"
 
-    #print u"ack(-1, 0) = ", ack(-1, 0)
-    #print u"ack(0, -1) = ", ack(0, -1)
+    # ack_tests values: m, n, expected result
+    ack_tests = ((-1, 0, None),
+                 (0, -1, None),
+                 (-1, -1, None),
+                 (-1, 1, None),
+                 (1, -1, None),
+                 (0, 0, 1),
+                 (0, 1, 2),
+                 (0, 2, 3),
+                 (0, 3, 4),
+                 (0, 4, 5),
+                 (1, 0, 2),
+                 (1, 1, 3),
+                 (1, 2, 4),
+                 (1, 3, 5),
+                 (1, 4, 6),
+                 (2, 0, 3),
+                 (2, 1, 5),
+                 (2, 2, 7),
+                 (2, 3, 9),
+                 (2, 4, 11),
+                 (3, 0, 5),
+                 (3, 1, 13),
+                 (3, 2, 29),
+                 (3, 3, 61),
+                 (3, 4, 125))
 
-    assert ack(0, 0) == 1
-    assert ack(0, 1) == 2
-    assert ack(0, 2) == 3
-    assert ack(0, 3) == 4
-    assert ack(0, 4) == 5
-
-    assert ack(1, 0) == 2
-    assert ack(1, 1) == 3
-    assert ack(1, 2) == 4
-    assert ack(1, 3) == 5
-    assert ack(1, 4) == 6
-
-    assert ack(2, 0) == 3
-    assert ack(2, 1) == 5
-    assert ack(2, 2) == 7
-    assert ack(2, 3) == 9
-    assert ack(2, 4) == 11
-
-    assert ack(3, 0) == 5
-    assert ack(3, 1) == 13
-    assert ack(3, 2) == 29
-    assert ack(3, 3) == 61
-    assert ack(3, 4) == 125
-
+    for m, n, expected in ack_tests:
+       assert ack(m, n) == expected
 
     print "All tests pass"
