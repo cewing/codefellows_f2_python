6051ac4da414c7187cc8ec27b7285f6d7f5cecbb
jefrailey <jcaetof@gmail.com>
2014-05-07 20:44:05 -0700

    Add input request and expand the fruit_list

--- students/JoshuaFrailey/list_lab.py
+++ students/JoshuaFrailey/list_lab.py
@@ -1,2 +1,11 @@
 fruit_list = ["Apples", "Pears", "Oranges", "Peaches"]
 print fruit_list
+user_fruit = raw_input(
+    """
+    Please enter a number corresponding to a fruit in the list (starting
+    at 1):
+    """
+    )
+print str(user_fruit) + str(fruit_list[user_fruit])
+fruit_list += ["Mangos"]
+print fruit_list
