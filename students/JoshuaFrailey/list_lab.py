1fcdd7f522c81ab19fc3a5c1dbb944c8333707d6
jefrailey <jcaetof@gmail.com>
2014-05-07 23:15:32 -0700

    Complete first draft of series 1 list creation

--- students/JoshuaFrailey/list_lab.py
+++ students/JoshuaFrailey/list_lab.py
@@ -1,11 +1,21 @@
-fruit_list = ["Apples", "Pears", "Oranges", "Peaches"]
+fruit_list = [u"Apples", u"Pears", u"Oranges", u"Peaches"]
 print fruit_list
 user_fruit = raw_input(
-    """
+    u'Please enter a fruit to add to the list: '
+    )  # Need to verify added fruit is a string
+fruit_list.append(user_fruit)
+user_picks_fruit = raw_input(
+    u"""
     Please enter a number corresponding to a fruit in the list (starting
     at 1):
-    """
+    """  # fix formatting
     )
-print str(user_fruit) + str(fruit_list[user_fruit])
-fruit_list += ["Mangos"]
+# Need to convert user_picks_fruit to int and subtract 1
+print str(user_picks_fruit) + str(fruit_list[user_picks_fruit])
+fruit_list += [u"Mangos"]
 print fruit_list
+fruit_list.insert(0, u"Papaya")
+
+for fruit in fruit_list:
+    if fruit.startswith(u'P'):
+        print fruit
