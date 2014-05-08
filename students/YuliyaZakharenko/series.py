a69bed7f38327ffefe8f995fc2ddd1750375ea47
Yuliya Zakharenko <rockroada@gmail.com>
2014-05-07 19:29:31 -0700

    committing fixing tabs to spaces

--- students/YuliyaZakharenko/series.py
+++ students/YuliyaZakharenko/series.py
@@ -1,32 +1,32 @@
 def fibonacci(n):
-	"""return nth value in fibonacci series"""
-	if n <= 0:
-		return "no value"
-	else:
-	    series = [0,1]
-	    for i in range(2, n):
-	        series.append(series[i-1] + series[i-2])
+    """return nth value in fibonacci series"""
+    if n <= 0:
+        return "no value"
+    else:
+        series = [0,1]
+            for i in range(2, n):
+                series.append(series[i-1] + series[i-2])
             return series[n-1]
 
 
 def lucas(n):
-	"""return nth value in lucas series"""
-	if n <= 0:
-		return "no value"
-	else:
-	    series = [2,1]
-	    for i in range(2, n):
-	        series.append(series[i-1] + series[i-2])
+    """return nth value in lucas series"""
+    if n <= 0:
+        return "no value"
+    else:
+        series = [2,1]
+        for i in range(2, n):
+            series.append(series[i-1] + series[i-2])
             return series[n-1]        
 
 def sum_series(n, x = 0, y = 1):
     """return nth value in fibonacci or lucas series or undefined series message"""
     if x == 0 and y == 1:
-		return fibonacci(n)
+        return fibonacci(n)
     elif x == 2 and y == 1:
-		return lucas(n)
+        return lucas(n)
     else:
-	    return "undefined series"
+        return "undefined series"
     
 if __name__ == "__main__":
     #testing fibonacci series
