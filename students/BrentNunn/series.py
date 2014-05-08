040bbdc685f9e9e14ef24475fc94bf0950d0a4a9
Brent Nunn <brentnunn@yahoo.com>
2014-05-07 19:00:04 -0700

    Added sum_series() tests

--- students/BrentNunn/series.py
+++ students/BrentNunn/series.py
@@ -2,6 +2,8 @@
 
 def sum_series(n, i = 0, j = 1):
     u"""Return the nth value of a number series consecutively adding i to j"""
+    u"""Default parameters result in the Fibonacci series"""
+
     if n < 1:
         return None
     elif n == 1:
@@ -25,9 +27,9 @@ def lucas(n):
 
 
 if __name__ == '__main__':
-    print u"Testing numerical series functions\n"
+    print u"Testing numerical series functions"
 
-    print u"Testing fibonacci series function"
+    print u"\nTesting fibonacci series function"
     # fib_tests values: n, expected result
     fib_tests = ((-1, None),
                  (0, None),
@@ -45,7 +47,7 @@ if __name__ == '__main__':
         assert fibonacci(n) == expected
 
 
-    print u"Testing Lucas numbers function\n"
+    print u"\nTesting Lucas numbers function"
     # lucas_tests values: n, expected result
     lucas_tests = ((-1, None),
                    (0, None),
@@ -62,6 +64,24 @@ if __name__ == '__main__':
         print u"assert lucas(", n, ") == ", expected
         assert lucas(n) == expected
 
-    print u"All tests pass"
+    print u"\nTesting sum_series function"
+    # sum_series_tests values: n, expected result
+    sum_series_tests = ((-1, 0, 1, None),
+                        (0, 2, 1, None),
+                        (1, 0, 1, 0),
+                        (2, 0, 1, 1),
+                        (3, 0, 1, 1),
+                        (4, 0, 1, 2),
+                        (5, 2, 1, 7),
+                        (6, 2, 1, 11),
+                        (7, 2, 1, 18),
+                        (8, 2, 1, 29))
+
+    for n, i, j, expected in sum_series_tests:
+        print u"assert sum_series(", n, ", i, ", i, " j, ", j, ") == ", 
+        print expected
+        assert sum_series(n, i, j) == expected
+
+    print u"\nAll tests pass"
 
 
