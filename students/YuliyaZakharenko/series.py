2cc28b2663476c07de8d9143fc48f547a515381b
Yuliya Zakharenko <rockroada@gmail.com>
2014-05-07 18:52:28 -0700

    committing moving assert statements out of the function into __name__==__main__ block and adding comments

--- students/YuliyaZakharenko/series.py
+++ students/YuliyaZakharenko/series.py
@@ -21,13 +21,15 @@ def lucas(n):
 
 def sum_series(n, x = 0, y = 1):
     """return nth value in fibonacci or lucas series or undefined series message"""
-	if x == 0 and y == 1:
+    if x == 0 and y == 1:
 		return fibonacci(n)
-	elif x == 2 and y == 1:
+    elif x == 2 and y == 1:
 		return lucas(n)
-	else:
-	    return "undefined series"	
-def test_fun():
+    else:
+	    return "undefined series"
+    
+if __name__ == "__main__":
+    #testing fibonacci series
     assert fibonacci(1) == 0
     assert fibonacci(2) == 1
     assert fibonacci(3) == 1
@@ -36,6 +38,7 @@ def test_fun():
     assert fibonacci(6) == 5
     assert fibonacci(7) == 8
     assert fibonacci(8) == 13
+    #testing lucas series
     assert lucas(1) == 2
     assert lucas(2) == 1
     assert lucas(3) == 3
@@ -44,6 +47,7 @@ def test_fun():
     assert lucas(6) == 11
     assert lucas(7) == 18
     assert lucas(8) == 29
+    #testing sum_series
     assert sum_series(5) == 3
     assert sum_series(6) == 5
     assert sum_series(7) == 8
@@ -53,7 +57,4 @@ def test_fun():
     assert sum_series(7, 2, 1) == 18
     assert sum_series(8, 2, 1) == 29
     assert sum_series(5, 3, 3) == "undefined series"    
-
     print "All Tests Pass" 
-if __name__ == "__main__":
-    test_fun()
