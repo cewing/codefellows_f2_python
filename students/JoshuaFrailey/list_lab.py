0d166a1b500d15bee6450af4750caaaf6b621991
jefrailey <jcaetof@gmail.com>
2014-05-08 19:44:45 -0700

    Replace _print_list() with _print_basket() to reduce print statements

--- students/JoshuaFrailey/list_lab.py
+++ students/JoshuaFrailey/list_lab.py
@@ -8,24 +8,32 @@ def _validate_input(prompt, accpet):  # Valid _input or Validate_input?
     return response
 
 
-def _print_list(list_):
+def _print_basket(list_):
+    print u"The basket contains:",
+    for ele in list_:
+        print ele,
+    print u"\n"
+
+def function():
     pass
 
 # Series 1:
 fruit_list = [u"Apples", u"Pears", u"Oranges", u"Peaches"]
-print fruit_list
-user_fruit = raw_input(u'Please enter a fruit to add to the list: ')
+_print_basket(fruit_list)
+user_fruit = raw_input(u'Please enter a fruit to add to the basket: ')
 fruit_list.append(user_fruit)
+_print_basket(fruit_list)
 user_picks_fruit = _validate_input(
     u"""Please enter a number corresponding to a fruit in the list (starting at 1): """,
-    str(range(1, len(fruit_list)))
+    str(range(1, len(fruit_list)+1))
     )
 print u"""You picked fruit number {}, {}""".format(
     user_picks_fruit, fruit_list[int(user_picks_fruit)-1]
     )
 fruit_list += [u"Mangos"]
-print fruit_list
+_print_basket(fruit_list)
 fruit_list.insert(0, u"Papaya")
+_print_basket(fruit_list)
 
 for fruit in fruit_list:
     if fruit.startswith(u'P'):
