07cc5ae5cc313eac4185afe580dfdfebf4f340e9
miracode <rascatim@gmail.com>
2014-05-09 09:16:12 -0700

    Rot13 ready

--- students/MichelleRascati/rot13.py
+++ students/MichelleRascati/rot13.py
@@ -1,2 +1,8 @@
 def rot13(x):
     """Return ROT13 encrypted text."""
+    y = x.encode('rot13')
+    return y
+
+if __name__ == '__main__':
+    assert rot13(u'The quick brown fox 123!?') == u'Gur dhvpx oebja sbk 123!?'
+    print 'All tests pass'
