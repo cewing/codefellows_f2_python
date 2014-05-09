71af445f12eab959c69368f84aac51fa2d23dc37
miracode <rascatim@gmail.com>
2014-05-09 11:42:45 -0700

    list_lab working 1

--- students/MichelleRascati/list_lab.py
+++ students/MichelleRascati/list_lab.py
@@ -1,4 +1,6 @@
-#!/usr/env/python
+#!/usr/bin/env python
+
+
 def list():
     """Create list of "Apples", "Pears", "Oranges", "Peaches"."""
     return [u"Apples", u"Pears", u"Oranges", u"Peaches"]
@@ -6,7 +8,9 @@ def list():
 
 def d_list(x):
     """Display a list (x)."""
-    print x
+    for val in x:
+        print val,
+    print
 
 
 def p_list(x, where):
@@ -21,18 +25,20 @@ def add_end(val, x):
 
 def n_list(x):
     """Ask user for value of list to return (x)."""
-    n = raw_input(u"What number fruit would you like?: ")
-    return u"Item %i: %s" % (n, x[n - 1])
+    n = int(raw_input(u"What number fruit would you like?: "))
+    print u"Item %i: %s" % (n, x[n - 1])
+    return None
 
 
 def add_plus(val, x):
     """Add value (val) to beginning of list (x) using the + operator."""
-    return val + x
+    return [val] + x
 
 
 def add_insert(val, x):
     """Add value (val) to beginning of list (x) using the append() fucntion."""
-    return x.insert(0, val)
+    x.insert(0, val)
+    return x
 
 
 def begin_with(char, x):
@@ -42,7 +48,7 @@ def begin_with(char, x):
             print val
 
 if __name__ == '__main__':
-    # Create a list that contains “Apples”, “Pears”, “Oranges” and “Peaches”.
+    # Create a list that contains "Apples", "Pears", "Oranges", "Peaches".
     fruits = list()
     # Display the list.
     d_list(fruits)
@@ -53,9 +59,11 @@ if __name__ == '__main__':
     # Ask the user for a number and display the number back to
     # the user and the fruit corresponding to that number.
     n_list(fruits)
-    # Add another fruit to the beginning of the list using “+” and display.
-    add_plus(p_list(fruits, u'beginning'), fruits)
-    # Add another fruit to the beginning of the list using insert() & display.
-    add_insert(p_list(fruits, u'beginning'), fruits)
-    # Display all the fruits that begin with “P”, using a for loop.
+    # Add another fruit to the beginning of the list using "+" and display.
+    fruits = add_plus(p_list(fruits, u'beginning'), fruits)
+    d_list(fruits)
+    # Add another fruit to the beginning of the list using insert() &l display.
+    fruits = add_insert(p_list(fruits, u'beginning'), fruits)
+    d_list(fruits)
+    # Display all the fruits that begin with "P", using a for loop.
     begin_with("P", fruits)
