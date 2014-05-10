2f96d7108ab2424b937e97b47f7c675f7f73c1b5
jefrailey <jcaetof@gmail.com>
2014-05-10 12:55:41 -0700

    Add flow control skeleton and expected functions

--- students/JoshuaFrailey/mailroom.py
+++ students/JoshuaFrailey/mailroom.py
@@ -11,4 +11,36 @@ for donor in donor_list:
     for donations in range(random.randint(1, 3)):
         donor.append(round(random.random()*10000, 2))
 
-# print donor_list
+
+def _get_donor():
+    pass
+
+
+def _get_donation():
+    pass
+
+
+def _send_thankyou():
+    pass
+
+
+def _create_report():
+    pass
+
+
+while True:
+    menu = u"""
+        Please select from the following:
+        '1: Send a Thank You'
+        '2: Create a Report'
+        '3: Exit'
+        """
+    input_ = raw_input(menu)
+    if input_.lower() in ['1', 's', 'send a thank you']:
+        _send_thankyou()
+    elif input_.lower() in ['2', 'c', 'create a report']:
+        _create_report()
+    elif input_.lower() in ['3', 'e', 'exit']:
+        break
+    else:
+        input_ = raw_input(u"Please enter '1', '2', or '3'")
