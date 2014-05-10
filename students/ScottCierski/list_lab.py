77cdcb6b119b824ff1bb59fed4ebb76d25d40456
Scott C <scott4@gmail.com>
2014-05-10 01:36:36 -0700

    Added initial pass at 'Do you like <fruit>?' section.

--- students/ScottCierski/list_lab.py
+++ students/ScottCierski/list_lab.py
@@ -10,31 +10,40 @@ def do_stuff():
     # fruit_number = raw_input('Please enter the number of a fruit in the list: ')
     # print fruit_list[int(fruit_number)-1]
 
-    print 'Adding "Grapes" to the end of the list (using "+")...'
-    grapes_list = ['Grapes']
-    fruit_list  = fruit_list + grapes_list
-    print fruit_list
+    # print 'Adding "Grapes" to the end of the list (using "+")...'
+    # grapes_list = ['Grapes']
+    # fruit_list  = fruit_list + grapes_list
+    # print fruit_list
 
-    print 'Adding "Plums" to the end of the list (using "insert")...'
-    fruit_list.insert(len(fruit_list), 'Plums')
-    print fruit_list
+    # print 'Adding "Plums" to the end of the list (using "insert")...'
+    # fruit_list.insert(len(fruit_list), 'Plums')
+    # print fruit_list
 
-    print 'Displaying all fruits beginning with "P"...'
-    for fruit in fruit_list:
-        if fruit[:1] =='P':
-            print fruit
+    # print 'Displaying all fruits beginning with "P"...'
+    # for fruit in fruit_list:
+    #     if fruit[:1] =='P':
+    #         print fruit
 
-    print 'Printing the current list...'
-    print fruit_list
+    # print 'Printing the current list...'
+    # print fruit_list
 
-    print 'Removing the last fruit from the list...'
-    fruit_list.pop()
-    print 'Printing the current list...'
-    print fruit_list
+    # print 'Removing the last fruit from the list...'
+    # fruit_list.pop()
+    # print 'Printing the current list...'
+    # print fruit_list
 
-    fruit_to_delete = raw_input('Please enter a fruit to delete from the list: ')
-    fruit_list.remove(fruit_to_delete)
-    print 'Printing the current list...'
+    # fruit_to_delete = raw_input('Please enter a fruit to delete from the list: ')
+    # fruit_list.remove(fruit_to_delete)
+    # print 'Printing the current list...'
+    # print fruit_list
+
+    fruit_list_copy = fruit_list[:]
+    for fruit in fruit_list_copy:
+        print 'Do you like ' + fruit.lower() + '?'
+        fruit_choice = raw_input()
+        if fruit_choice.lower() == 'no':
+            fruit_list.remove(fruit)
+            print fruit_list
 
 
 if __name__ == '__main__':
