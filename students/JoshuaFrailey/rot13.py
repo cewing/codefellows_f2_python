b7b6bebdddad7a11b4903c5b29474c8980b3f4f4
jefrailey <jcaetof@gmail.com>
2014-05-09 17:34:09 -0700

    Add translation of user prompt

--- students/JoshuaFrailey/rot13.py
+++ students/JoshuaFrailey/rot13.py
@@ -1,7 +1,7 @@
 import string
 
 # construct table
-alpha_upper = ''
+alpha_upper = ""
 for i in range(65, 91):
     alpha_upper += chr(i)
 alpha13_upper = alpha_upper[13:] + alpha_upper[:13]
@@ -10,3 +10,14 @@ alpha13_lower = alpha13_upper.lower()
 
 table_upper = string.maketrans(alpha_upper, alpha13_upper)
 table_lower = string.maketrans(alpha_lower, alpha13_lower)
+
+user_text = raw_input(u"Enter text to translate: ")
+translation = ''
+for letter in user_text:
+    if not letter.isalpha():
+        translation += letter
+    elif letter.isalpha() and letter.isupper():
+        translation += letter.translate(table_upper)
+    else:
+        translation += letter.translate(table_lower)
+print "The ROT13 translation is: {}".format(translation)
