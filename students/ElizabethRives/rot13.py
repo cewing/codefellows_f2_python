6dda06eaaab9108a4d2d6e35e6498a893a562162
rivese <erives@umich.edu>
2014-05-10 01:59:17 -0700

    rot13 assignment ready for review

--- students/ElizabethRives/rot13.py
+++ students/ElizabethRives/rot13.py
@@ -1,29 +1,58 @@
-
+# -*- coding: utf-8 -*-
 
 
 from string import maketrans
 
-def rot13():
-	"""Replace each letter in a text by the letter 13 away from it."""
+def rot13(s):
+	u"""Replace each letter in a text by a letter 13 letters after it in the alphabet."""
 
-	pass_in = 'abcdefghijklmnopqrstuvwxyz'
-	pass_out = 'nopqrstuvwxyzabcdefghijklm'
+	pass_in = u'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	pass_out = u'nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM'
 	table = maketrans(pass_in, pass_out)
 
-	s = "this is a very long string that needs To!"
-	
-
-
 	s_rot13 = s.translate(table)
-	print s_rot13
-
-rot13()
+	return s_rot13
 
 
-
-
+if __name__ == '__main__':
+	u"""Test that rot13 function performs as expected."""
 	
-	
+	vals = [
+
+		('a', 'n'),
+		('b', 'o'),
+		('c', 'p'),
+		('d', 'q'),
+		('e', 'r'),
+		('f', 's'),
+		('g', 't'),
+		('h', 'u'),
+		('i', 'v'),
+		('j', 'w'),
+		('k', 'x'),
+		('l', 'y'),
+		('m', 'z'),
+		('A', 'N'),
+		('B', 'O'),
+		('C', 'P'),
+		('D', 'Q'),
+		('E', 'R'),
+		('F', 'S'),
+		('G', 'T'),
+		('!', '!'),
+		(' ', ' '),
+
+	]
+
+	for s, output in vals:
+		assert rot13(s) == output
+	print u'All Tests Passed'
+
+
+
+
+
+
 
 
 
