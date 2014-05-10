29cff6fe77dff37254b10400a61ac37b9f3f0393
Brent Nunn <brentnunn@yahoo.com>
2014-05-10 07:34:18 -0700

    list_lab.py series 1 update

--- students/BrentNunn/list_lab.py
+++ students/BrentNunn/list_lab.py
@@ -3,43 +3,56 @@
 fruits = [u"Apples", u"Pears", u"Oranges", u"Peaches"]
 
 def print_fruits(fruits):
+    """Print all fruits in the sequence"""
     for fruit in fruits:
         print fruit    
 
 def get_fruit(prompt):
-    fruit_in = None
-
-    while not fruit_in:
+    """Get the name of another fruit."""
+    while True:
         fruit_in = raw_input(prompt)
         if fruit_in.isalpha():
             break
         else:
             print(u"Letters only please.")
-            fruit_in = None
 
-    return fruit_in
+    return unicode(fruit_in)
 
 def get_num(max_num):
-    num_in = None
-
-    while not num_in:
-        num_in = raw_input(u"Enter a number between 1 and {}: ".format(max_num))
+    """Get a number in the range of 1 to passed parameter"""
+    while True:
+        num_in = raw_input(u"\nEnter a number between 1 and {}: ".format(max_num))
         if num_in.isdigit():
             num_in = int(num_in)
             if num_in >= 1 and num_in <= max_num:
                 break
             else:
                 print u"Please limit your entry to the range specified."
-                num_in = None
         else:
             print u"Numbers only please."
-            num_in = None
 
-    return int(num_in)
+    return num_in
 
-
+# list_lab series 1
 print_fruits(fruits)
-fruits.extend([get_fruit(u"Please add another fruit to the list: ")])
+fruits.extend([get_fruit(u"\nPlease add another fruit to the list: ")])
 print_fruits(fruits)
 
 fruit_num = get_num(len(fruits))
+print u"Fruit number {} is {}.".format(fruit_num, fruits[fruit_num - 1])
+
+one_fruit = [get_fruit(u"\nPlease add another fruit to the list: ")]
+fruits = one_fruit + fruits
+print_fruits(fruits)
+
+one_fruit = get_fruit(u"\nPlease add another fruit to the list: ")
+fruits.insert(0, one_fruit)
+print_fruits(fruits)
+
+print u"\nThese fruits begin with 'P':"
+for fruit in fruits:
+    if fruit[0] == 'P':
+        print fruit
+
+# list_lab series 2
+
