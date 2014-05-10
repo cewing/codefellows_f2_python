94a799db096ef7e15484fe9c5e9bb83a5ffdaf0a
Scott C <scott4@gmail.com>
2014-05-10 16:45:27 -0700

    Added assert block, and now returning value from rot13() function back to main block.

--- students/ScottCierski/rot13.py
+++ students/ScottCierski/rot13.py
@@ -1,19 +1,36 @@
 import string
 
-def rot13():
+def rot13(input_string):
     """Perform ROT13 encryption"""
-
-    input_string = raw_input('Please enter a string: ')
-    print u""
 
     translate_table = string.maketrans( "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
     "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
 
     rot_13_input_string = string.translate(input_string, translate_table)
-
-    print u"Printing ROT13 for this string..."
-    print rot_13_input_string
-    print u""
+    return rot_13_input_string
 
 if __name__ == '__main__':
-    rot13()
+
+    # Assert block
+    # Test all lower case
+    assert rot13('amq') == 'nzd'
+
+    # Test all upper case
+    assert rot13('BGW') == 'OTJ'
+
+    # Test mix of lower case, upper case
+    assert rot13('fRx') == 'sEk'
+
+    # Test mix of lower case, upper case, numbers
+    assert rot13('J5oL8') == 'W5bY8'
+
+    # Test mix of lower case, upper case, numbers, white space, special characters
+    assert rot13('dS 4e!T @y*') == 'qF 4r!G @l*'
+
+    # Accept user input
+    input_string = raw_input('Please enter a string: ')
+    print u""
+
+    print u"Printing ROT13 for this string..."
+    rot_13_input_string = rot13(input_string)
+    print rot_13_input_string
