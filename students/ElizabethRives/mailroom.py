d649b6f77d327091416a9c8813f21404736b6682
rivese <erives@umich.edu>
2014-05-10 17:28:52 -0700

    Added more structure

--- students/ElizabethRives/mailroom.py
+++ students/ElizabethRives/mailroom.py
@@ -1,40 +1,76 @@
 
 
-def create_dictionary():
-	d = {}
+
+def create_database():
+	d = []
 	return d
 	
-create_dictionary()
+create_database()
+
+database = create_database()
 
 
-database = create_dictionary()
+def menu():
+	action = raw_input(u'Send a Thank You or Create a Report?')
+	return action
 
-def get_name():
-	name = raw_input("Enter a first and last name.")
-	if name == 'list':
-		return database
-		name = raw_input("Enter a first and last name")
-	if name not in database:
-		database.append(name)
-		return name
-	return name
+menu()
 
-get_name()
+choice = menu()
+
+while choice == 'Send a Thank You':
+	
+
+	def get_name():
+		input_name = raw_input(u'Enter a first and last name')
+		if input_name == 'list':
+			return database
+			input_name = raw_input(u'Enter a first and last name')
+			return input_name
+		if input_name not in database:
+			database.append(input_name)
+			return input_name
+		return input_name
+
+	get_name()
+
+	name = get_name()
 
 
-def get_amount():
-	amount = raw_input("Enter a donation amount")
-	if amount == 'this' :
-		amount = raw_input("enter amount")
-		database.append(amount)
-	else:
-		database.append(amount)
-		return amount
+	def get_amount():
+		input_amount = raw_input(u'Enter a donation amount')
+		if input_amount == 'this':
+			input_amount = raw_input(u'Enter a donation amount')
+			database.append(input_amount)
+		else:
+			database.append(input_amount)
+			return input_amount
 		
-get_amount()
+	get_amount()
+
+	amount = get_amount()
 
 
-def write_email():
-	print "xxxxx"
+	def write_email():
+		print u"xxxxx"
 
-write_email()
+	write_email()
+
+
+else: 
+
+
+	def print_report():
+		print u'yyyyyyyy'
+
+	print_report()
+
+
+
+
+
+
+
+	
+
+
