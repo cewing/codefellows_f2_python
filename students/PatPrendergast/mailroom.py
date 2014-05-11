2e9235749e5512b8a543941790684b06e874c3a1
Pat Prendergast <pat@wetdog.net>
2014-05-11 13:42:09 -0700

    Mailroom.py, continued work.

--- students/PatPrendergast/mailroom.py
+++ students/PatPrendergast/mailroom.py
@@ -1,78 +1,128 @@
 #!usr/bin/env python
 
 #mailroom.py
-''' Mail Merge:  a program for tracking and resonding to donors and donations'''
+
+#Mail Merge:  a program for tracking and resonding to donors and donations
 
 
 def start():
+    ''' Show menu of actions, recieve user instructions on how to proceed '''
     print top_menu
     action = raw_input('What would you like to do: ')
     action.lower()
     if action == u'l' or action == u'list':
         name_list(donors)
         start()
+    elif action == u'a' or action == u'add':
+        add_donor()
+        start()
     elif action == u'm' or action == u'mail':
         mail()
+        start()
     elif action == u'r' or action == u'report':
         report(donors)
+        start()
     elif action == u'q' or action == u'quit':
         exit(0)
     else:
         print u'Did not recognize that response.'
         start()
 
-def mail():
-    donor_name = raw_input(u'Which person do you wish to thank today? ')
-    donor_info(donor_name)
-    print message
 
-def report(t):
-    table_top= u'Name\t\t\t\tTotal Donation Amount\n'
-    print table_top
+# Defining functions for the actions in start()
+def mail():
+    ''' Show donor list, ask who to message, return message with name and last donation '''
+    name_list(donors)
+    donor_name = raw_input(u'Which person do you wish to thank today (Full Name)? ')
+    print message.format(name=recipient(donor_name), last_donation=last_donation(donor_name))
+
+
+def recipient(d):
+    ''' Return user name for messageing, or add a user if not there '''
+    for e in donors:  
+        name = e[0]
+        if d == name:
+            return name 
+        
+
+def last_donation(d):
+    ''' Return last donation from a donor '''
     for e in donors:
-        print donors[e][0] + '\t\t' + sum(donors[e][1:])
+        name = e[0]
+        last_donation = e[-1]
+        if d == name:
+            return last_donation  
+
 
 def name_list(t):
-    for e in range(t):
-        print donors[e][0] # accessing list in list?
+    ''' Return a list of donor names (only) '''
+    for e in t:
+        print e[0]
+    
 
-def donor_info(d):
-    if d in donors:
-        donors.index(d)
-        return donors[d][0]  # accessing list in list?
+def report(t):
+    ''' Print report: Donor name and Total Amount Given '''
+    print ''
+    table_top = u'Name\t\t\t\tTotal Donation Amount\n'
+    print table_top
+    for e in t:
+        name = e[0]
+        donations = sum(e[1:])
+        print u"%s\t\t\t$%s" % (name, donations)
+
+
+def add_donor():
+    ''' Add a new donor to donors list: Name, amount of donation '''
+    print u"Adding a new donor to our Donor's List."
+    print''
+    new_donor = raw_input("Who are we adding (Full Name)? >")
+    print''
+    donation = int(raw_input(u'How much did they give? >'))
+    print''
+    if donation > 0:  #need to add the erroneus input response here.
+        donors.append([new_donor, int(donation)])
+        print u'{new_donor} has been added.'.format(new_donor=new_donor)
+        print''
+        '''thanks = raw_input(u"Do you wish to mail a thank you right now? Y or N> "
+        if thanks == u'y' or thanks == u'Y':
+            print message.format(name=recipient(new_donor), last_donation=last_donation(donation))
+        else:
+            start()'''
     else:
-        add_donor(d)
-
-def add_donor(d):
-    donors.append([d])
-    donation = int(raw_input(u'How much did they give? '))
-    donors.append()
+        print"That is not a number. Donor not added"
+        add_donor()
 
 
+
+donors = [  [u'Peter Parker', 10, 20, 30], 
+            [u'Betty Boop', 30, 50], 
+            [u'Bruce Wayne', 10000, 20000, 4000],
+            [u'Mickey Mouse', 1, 2, 3],
+            [u'Tony Stark', 20000, 40000, 60000, 80000],
+            [u'Diana Prince', 250, 400, 500]
+]
+
+
+# Strings for different User Actions
 top_menu = u'''
-Pythonic Florida Foundation - Thinning out Fat Code for YOU!
+The Pythonic Foundation - Thinning out Fat Code for YOU!
 
-Welcome to Mail Merge - Top Menu
+WELCOME TO THE MAIL MERGE - TOP MENU
 
 Intsructions - Type the first letter of the following:
 
 List - to see a list of donor names
+Add - to add a donor to the list of donors
 Mail - to send a prewritten message to a donor
 Report - to see all donors listed by amount given
 Quit - to leave the program.
 
 '''
 
-donors = [  [u'Peter Parker', 10, 20, 30], 
-            [u'Betty Boop', 30, 50], 
-            [u'Bruce Wayne', 100000, 2000000, 400000],
-            [u'Mickey Mouse', 1, 2, 3],
-            [u'Tony Stark', 20000, 40000, 60000, 80000]]
-
 message = u"""
-Though we may be out killing Burmese Pythons, we still
-have time to thank you, %s, for your exceedingly 
-generous $%s donation.  Though we endeavor to be self sufficient 
+Though we may be out canvassing various code fat farms, we still
+have time to thank you, {name}, for your exceedingly 
+generous ${last_donation} donation.  Though we endeavor to be self sufficient 
 in our mission, selling our code drying services to the highest 
 bidder, we can always take moment to recognize you with one of 
 our self agrandizing emails.  
@@ -84,18 +134,15 @@ Thanks kindly,
 Bruce Banner
 Executive Director
 
-""" % (donors[0][0], donors[0][-1])
+"""
 
-#start()
 
-print message
+if __name__ == '__main__':
+    start()
 
-#if __name__ == '__main__':
-    #start()
 
-name_list(donors)
 
-report(donors)
+
 
 
 
