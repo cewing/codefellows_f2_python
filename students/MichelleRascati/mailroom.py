1027980ad0e96f93e40965fef9f56a1ef9da8563
miracode <rascatim@gmail.com>
2014-05-11 13:40:29 -0700

    mailroom thank you working

--- students/MichelleRascati/mailroom.py
+++ students/MichelleRascati/mailroom.py
@@ -2,35 +2,50 @@
 
 
 def thanks(ty_name):
-    """What this does"""
+    """Prompt for donation amount and return amount"""
     amt = raw_input("What is %s's donation amount? " % ty_name)
-    while not amt == float(amt):
-        amt = raw_input("%s is not a number, please enter donation \
-            amount: " % amt)
+    while True:
+        try:
+            amt = float(amt)
+        except ValueError:
+            amt = raw_input("%s is not a number, please enter donation \
+amount: " % amt)
+        else:
+            break
+    #while not amt == float(amt):
+    #    amt = raw_input("%s is not a number, please enter donation \
+    #        amount: " % amt)
     print "Dear %s, \n \
-        Thank you for your donation of $%f to our charity.  We appreciate\
-        your support. \n \
-        Sincerely, \n Michelle Rascati" % (ty_name, amt)
+Thank you for your donation of $%.2f to our charity.  We appreciate \
+your support. \n \
+Sincerely,\n Michelle Rascati" % (ty_name, amt)
+    return amt
 
 
 if __name__ == '__main__':
-    donations = [['Larry', 10.00, 150.50, 75.00],
-                ['Sue', 40.00, 35.00],
-                ['Julie', 35.50],
-                ['Bob', 60.25, 100.00]
-                ['Karen', 83.50, 72.45, 90.25]]
+    donations = [[u'Larry', 10.00, 150.50, 75.00],
+                [u'Sue', 40.00, 35.00],
+                [u'Julie', 35.50],
+                [u'Bob', 60.25, 100.00],
+                [u'Karen', 83.50, 72.45, 90.25]]
 do = u''
 while do not in (u'Send a Thank You', u'Create a Report'):
-    u'' + raw_input("'Send a Thank You' or 'Create a Report'? ")
+    do = u'' + raw_input("'Send a Thank You' or 'Create a Report'? ")
+
 if do == u'Send a Thank You':
     # Prompt for full name
-    name = u'' + raw_input("Type full name for Thank You letter or 'list'\
-        for a list of names: ")
-    if name == u'list':
-        for val in donations:
-            print val[0]
-    elif name not in [col[0] for col in donations]:
-        donations.append([name])
-        thanks(name)
-    else:
-        thanks(name)
+    name = ''
+    while name not in [col[0] for col in donations]:
+        name = u'' + raw_input("Type full name for Thank You letter or 'list'\
+for a list of names: ")
+        if name == u'list':
+            for val in donations:
+                print val[0]
+        elif name not in [col[0] for col in donations]:
+            donations.append([name])
+    donations[[col[0] for col in donations].index(name)].append(thanks(name))
+
+elif do == 'Create a Report':
+    for i in donations:
+        #TODO sort by donation amt
+        print i
