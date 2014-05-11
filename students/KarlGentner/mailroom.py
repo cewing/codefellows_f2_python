5d1081d4d02b2698014257139b0f93eb6aa0c37a
kgentner <gentnerkarl@gmail.com>
2014-05-11 14:55:17 -0700

    added starting data and user send/create

--- students/KarlGentner/mailroom.py
+++ students/KarlGentner/mailroom.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python
+
+
+# Create original donor list with names and past donations (in dollars)
+donors = [[u'Amy Akin', [1]],
+          [u'Bob Bueller', [10, 20]],
+          [u'Carol Carlson', [100, 200, 300]],
+          [u'Dave Davis', [1000, 2000]],
+          [u'Eve Eastman', [10000]]]
+
+
+# User pick "Send a Thank You" or "Create a Report"
+def userSendCreate():
+    userInput = raw_input("type 'S' to send a thank you" +
+                          " - OR - type 'C'  to create a report-->").decode()
+    userInput = userInput.capitalize()
+    while userInput != 'S' and userInput != 'C':
+        userInput = raw_input("type 'S' to send a thank you" +
+                              " - OR - type 'C'  to create a report-->").decode()
+    return userInput
