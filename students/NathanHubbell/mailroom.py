3bb3220e4b9b9dfe753617e343602cc1d25ce977
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-11 16:04:47 -0700

    breaking functionality up into more functions.

--- students/NathanHubbell/mailroom.py
+++ students/NathanHubbell/mailroom.py
@@ -5,31 +5,38 @@ def donors(donorsANDdollars):
     """Generate list of donors from main data structure."""
     donorsList = []
     for i in range(len(donorsANDdollars)):
-        print donorsANDdollars[i][0]
         donorsList.append(donorsANDdollars[i][0])
     return donorsList
 
-def send_ThankYou():
-    sent = 0
-    while sent == 0:
-        theInput=raw_input(u"Enter a new name or one on the list to send a thank you. Type 'list' to see the list. Name :")
-        if theInput in donorsANDdollars[1]:
-            print theInput
+def selectDonor():
+    while True:
+        theInput=raw_input(u"Enter a new name or one on the list to send a thank you. Type 'list' to see the list. Name: ")
+        if theInput == u"list":
+            print donors(donorsANDdollars)
+        elif theInput in donors(donorsANDdollars):
+            return theInput
+        elif theInput not in donors(donorsANDdollars):
+            donorsANDdollars.append([theInput])
+            return theInput
+
+def getDonationAmount(theDonor):
+    while True:
+        theInput=raw_input(u"Enter a donation amount as a number: ")
+
 
 def create_Report():
     pass
 
 
 
-
-print donors(donorsANDdollars)
-#for i in range(len(donorsANDdollars)):
-#answered=0
-#while answered == 0:
-#    choice = raw_input(u"Please input 1 or 2. Send a Thank You(1) or Create a Report(2): ")
-#    if choice == u"1":
-#        send_ThankYou()
-#        answered=1
-#    elif choice == u"2":
-#        create_Report()
-#        answered=1
+for i in range(len(donorsANDdollars)):
+    answered=0
+    while answered == 0:
+        choice = raw_input(u"Please input 1 or 2. Send a Thank You(1) or Create a Report(2): ")
+        if choice == u"1":
+            selectDonor()
+            send_ThankYou()
+            answered=1
+        elif choice == u"2":
+            create_Report()
+            answered=1
