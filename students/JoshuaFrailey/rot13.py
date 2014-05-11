17d886654043be077f8b3771450607f7eed8f2cb
jefrailey <jcaetof@gmail.com>
2014-05-11 13:54:11 -0700

    Redefine _rot13_table() to use string.ascii... instead of for loops to generate initial strings for readability

--- students/JoshuaFrailey/rot13.py
+++ students/JoshuaFrailey/rot13.py
@@ -2,13 +2,10 @@ import string
 
 
 def _rot13_table():
-    """Return rot13 encrpytion table for string.translate()"""
-    alpha = u""
-    for i in range(65, 91):
-        alpha += unichr(i)
+    u"""Return rot13 encrpytion table for string.translate()"""
+    alpha = string.ascii_lowercase
     alpha13 = alpha[13:] + alpha[:13]
-    for i in range(97, 123):
-        alpha += unichr(i)
+    alpha += string.ascii_uppercase
     alpha13 = alpha13 + alpha[-13:] + alpha[-26:-13]
     return string.maketrans(alpha, alpha13)
 
@@ -18,18 +15,19 @@ def rot13(text, table=_rot13_table()):
     return text.translate(table)
 
 if __name__ == "__main__":
+
     # Generate translation table and convert common characters using rot13().
     table = _rot13_table()
     vals = []
     for i in (range(32, 127)):
-        input_ = chr(i)  # Unsure why it won't work if input is unichr(i)
+        input_ = chr(i)  # Unsure why it won't work if input is unichr(i).
         trans = rot13(input_)
         trans_ord = ord(trans)
         vals.append((input_, i, trans, trans_ord))
 
     # Assert that alpha characters are translated by "rotating" the position
-    # of the character by 13. Assert that punctuation and whitespace are
-    # not changed. Print the translation of each character so that the user
+    # of the character by 13.  Assert that punctuation and whitespace are
+    # not changed.  Print the translation of each character so that the user
     # can visually verify output.
     for val in vals:
         ord_diff = abs(val[1] - val[3])
@@ -42,6 +40,6 @@ if __name__ == "__main__":
     # Allow the user to translate a phrase of their choosing to further
     # demonstrate that the function meets the specification.
     user_text = raw_input(u"Enter text to translate: ")
-    print u"The ROT13 translation of {} is: {}".format(
-        user_text, rot13(user_text, table)
-        )
+    msg = u"The ROT13 translation of "
+    msg += u"{} is: {}".format(user_text, rot13(user_text, table))
+    print msg  # Is there a better way to format this for code readability?
