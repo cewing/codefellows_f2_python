74d7e6e62d8e3819c12041bf0547e62e5fd13622
rivese <erives@umich.edu>
2014-05-12 00:13:42 -0700

    Fixed to be 1 is first basis

--- students/ElizabethRives/list_lab.py
+++ students/ElizabethRives/list_lab.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
 
 
-l = ['Apples', 'Pears', 'Oranges', 'Peaches']
+basket = ['Apples', 'Pears', 'Oranges', 'Peaches']
 
 
 def add_fruit():
 	u"""Ask user to add fruit to the list, also insert two list items."""
 	
-	print l
+	print basket
 	fruit = raw_input(u"What's a fruit you'd like to add to the list?")
-	l.append(fruit)
-	print l
+	basket.append(fruit)
+	print basket
 	
 	number = raw_input(u'Choose a number')
-	fruit_index = l[int(number)] 
+	fruit_index = basket[int(number) - 1] 
 	print number, fruit_index 
 
-	new_l = ['Plums'] + l
-	print new_l
-	new_l.insert(0, 'Grapes')
-	print new_l
+	new_basket = ['Plums'] + basket
+	print new_basket
+	new_basket.insert(0, 'Grapes')
+	print new_basket
 
-	for i in new_l:
+	for i in new_basket:
 		if i[0] == 'P':
 			print i
 
@@ -31,16 +31,16 @@ add_fruit()
 def remove_fruit():
 	u"""Remove last list item and ask user which fruit to remove."""
 	
-	print l
-	l.pop()
-	print l
-	new_l = l * 2
+	print basket
+	basket.pop()
+	print basket
+	new_basket = basket * 2
 
-	for i in new_l[:]:
+	for i in new_basket[:]:
 		bye = raw_input(u'Which fruit would you like to remove?')
-		if bye in new_l:
-			new_l[:] = (i for i in new_l if i != bye)
-		print new_l
+		if bye in new_basket:
+			new_basket[:] = (i for i in new_basket if i != bye)
+		print new_basket
 		break 
 	
 remove_fruit()
@@ -49,13 +49,13 @@ remove_fruit()
 def like_dislike():
 	u"""For each fruit in the list, modify list based on user input."""
 	
-	for i in reversed(range(len(l[:]))):
-	    like = raw_input(u'Do you like ' + l[i].lower()) 
+	for i in reversed(range(len(basket[:]))):
+	    like = raw_input(u'Do you like ' + basket[i].lower()) 
 	    if like == u'no':
-	    	l.remove(l[i])
+	    	basket.remove(basket[i])
 	    elif like != 'yes':
 	    	print u'Please only respond with yes or no'
-	print l
+	print basket
    
 like_dislike()
 
@@ -63,10 +63,10 @@ like_dislike()
 def final():
 	u"""Reverse items in the list and delete last item."""
 	
-	new_l = [i[::-1] for i in l]
-	l.pop()
-	print l
-	print new_l
+	new_basket = [i[::-1] for i in basket]
+	basket.pop()
+	print basket
+	print new_basket
 
 final()
 
