a72b6700f0d0617ca3af0ecdd40c1a57330a03d4
Jay Tyler <jmtyler@gmail.com>
2014-05-11 21:00:44 -0700

    Added tests to rot13.py

--- students/JasonTyler/mailroom.py
+++ students/JasonTyler/mailroom.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """Mailroom assignment"""
 
 # Each sublist referred to as a 'donor record'
@@ -6,9 +8,10 @@ donor_list = [
     ["Jane", "Goodall", 324, 234],
     ["Bob", "Hunter", 2, 3],
     ["Margie", "Smith", 3],
-    ["Jarrell", "Tommy", 56],
+    ["Tommmy", "Jarrel", 56],
     ["Ryan", "McKash", 2345, 234, 1234]
 ]
+
 
 def make_choice():
     """Prompt user to choose an action"""
@@ -23,6 +26,7 @@ def make_choice():
     elif user_choice == "q": quit_mailroom()
     else: make_choice()
 
+
 def send_thankyou():
     """Format 'Thank You' letter to send to donor"""
     print "Make a choice"
@@ -31,26 +35,19 @@ def send_thankyou():
     print "b to Return Back"
  
     user_choice = get_input()
+    donor = get_donor_from_str(user_choice)
+    donor_name = parse_str_to_donor_name(user_choice)
+    first, last = donor_name
     if user_choice == "b": make_choice()
-    elif len(user_choice).split(" ") == 2:
-        first = user_choice[0]
-        last = user_choice[1]
-        donor = get_donor(first, last)
-        if not donor:
-            get_donation()
-            create_donor(first, last)
-        else:
-            while True:
-                print "Enter donation amount"
-                donation = get_input()
-                if isintance(donation, (int, float)):
-                    add_donation(donor, int(donation))
-                else:
-                    continue
-
-
-    else: send_thankyou()
+    elif donor:
+        add_donation(donor, get_donation())
+    elif first and last:
+        donor = create_donor(donor_name)
+        add_donation(donor, get_donation())
+    else: 
+        send_thankyou()
     # Exit function with make_choice call
+
 
 def create_report():
     """Create a report of all donor donations"""
@@ -58,51 +55,91 @@ def create_report():
     # Exit function with make_choice call
     make_choice()
 
+
+def create_donor((first_name, last_name), donation=None):
+    """Create a new donor and return reference to donor record"""
+    pass
+    # if donation=None, don't add donation as a member
+    # also, don't add donor if first_name, last_name point to None
+
+def get_donor((first_name, last_name)):
+    """Use first_name and last_name to return a donor record"""
+    for donor_record in donor_list:
+        if donor_record[0] == first_name and donor_record[1] == last_name:
+            return donor_record
+    else: return None
+
+
+def get_donor_from_str(string):
+    """Get donor record from a string"""
+    name  = parse_str_to_donor_name(string)
+    return get_donor(name)
+
+
+def parse_str_to_donor_name(string):
+    """Take a string and return first_name, last_name"""
+    name_list = string.split(" ")
+    for element in name_list[:]:
+        if element == " " or not element:
+            del element
+        else:
+            pass
+    if len(name_list) == 2:
+        first, last = name_list[0], name_list[1]
+        return (first, last)
+    else:
+        return None
+
+
+def get_donation():
+    """Prompt for and validate a donation amount"""
+    while True:
+        print "Enter a donation amount"
+        donation = get_input()
+        try:
+            donation = int(donation)
+        except ValueError:
+            get_donation()
+        else:
+            return donation
+
+
+def add_donation(donor_record, donation):
+    """Add a donation to a donor record"""
+    pass
+
+
+def get_average_donation(donor_record):
+    """Get average donation from donor record"""
+    pass
+
+
+def get_total_donation(donor_record):
+    """Get total donation from donor record"""
+    pass
+
+
+def print_all_donor_names():
+    """Print list of donor names to terminal"""
+    names = list()
+    for i in donor_list:
+        formatted_name = "%s %s" % i[0], i[1]
+        names.append(formatted_name)
+    print names
+    del names
+
+
+def get_input():
+    """Get user input"""
+    return raw_input(">>> ").strip()
+
+
 def quit_mailroom():
     """Exit out of mailroom"""
     
     # Exit function with make_choice call
     make_choice()
 
-def create_donor(first_name, last_name):
-    """Create a new donor and return reference to donor record"""
-    pass
-
-def get_donor(first_name, last_name):
-    """Use first_name and last_name to return a donor record"""
-    for donor_record in donor_list:
-        if donor_record[0] == first_name and donor_record[1] == last_name:
-            return donor_record
-    else: return None 
-
-def get_donor_from_str(first_name, last_name):
-    """Get donor record from a string"""
-    pass
-
-def add_donation(donor_record, donation):
-    """Add a donation to a donor record"""
-    pass
-
-def get_average_donation(donor_record):
-    """Get average donation from donor record"""
-    pass
-
-def get_total_donation(donor_record):
-    """Get total donation from donor record"""
-    pass
-
-def print_all_donor_names():
-    """Print list of donor names to terminal"""
-    names = list()
-    for i in donor_list:
-        formatted_name = "%s %s" % i[0], i[1]    
-        names.append(formatted_name)
-    print names
-    del names
-
-def get_input():
-    """Get user input"""
-    return raw_input(">>> ").strip()
 
 # Start everything off with an initial call
 make_choice()
--- students/JasonTyler/rot13.py
+++ students/JasonTyler/rot13.py
@@ -12,5 +12,14 @@ def rot13(in_str):
 
 
 if __name__ == "__main__":
-    test_text = "How can you tell an extrovert from an introvert at NSA?\
-                 Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf."
+    test_text = \
+        "How can you tell an extrovert from an introvert at NSA? Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf."
+
+    test_text_decrypt = \
+        "Ubj pna lbh gryy na rkgebireg sebz na vagebireg ng AFN? In the elevators, the extrovert looks at the OTHER guy's shoes."
+
+    # testing for the symmetry of rot13; two operations should cycle back into original text
+    assert rot13(rot13(test_text)) == test_text
+    # testing verbatim translation
+    assert rot13(test_text) == test_text_decrypt
+    print "rot13 tests passed ok."
