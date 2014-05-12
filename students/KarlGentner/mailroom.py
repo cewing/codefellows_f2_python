71a7a9adbbbfc5cfff0c7461b09e236de968ec1f
kgentner <gentnerkarl@gmail.com>
2014-05-12 13:07:41 -0700

    better separation of functions and improved currency handling

--- students/KarlGentner/mailroom.py
+++ students/KarlGentner/mailroom.py
@@ -4,18 +4,18 @@
 import copy
 
 
-# Create original donor list with names and past donations (in dollars)
-donors = [[u'Amy Akin', [200, 450, 565]],
-          [u'Bob Bueller', [3234, 763489]],
-          [u'Carol Carlson', [101, 200, 300]],
-          [u'Dave Davis', [1000000, 20]],
-          [u'Eve Eastman', [12345]]]
+# Original donor list with names and past donations
+donors = [[u'Amy Akin', [200.32, 450.12, 565.24]],
+          [u'Bob Bueller', [3234.44, 76348.03]],
+          [u'Carol Carlson', [101.78, 201.92, 303.89]],
+          [u'Dave Davis', [100.45, 20.45]],
+          [u'Eve Eastman', [1200.00]]]
 
 
-# isInt - validate whether user input string is an integer
-def isInt(userInput):
+# isFloat helper - validate whether string can be cast into a float
+def isFloat(str):
     try:
-        int(userInput)
+        float(str)
         return True
     except ValueError:
         return False
@@ -30,38 +30,8 @@ def printDonors():
     print ("\n")
 
 
-# Print report
-def printReport():
-    report = copy.deepcopy(donors)
-    for i in range(len(report)):
-        donations = report[i][1]
-        total = 0
-        for j in range(len(donations)):
-            total += donations[j]
-        num = len(donations)
-        average = total / num
-        report[i].insert(1, str(average))
-        report[i].insert(1, str(num))
-        report[i].insert(1, str(total))
-    x = sorted(report, key=lambda donor: int(donor[1]))
-    print ("NAME\t\t\t\tTOTAL DONATIONS\t\tNUM OF DONATIONS\tAVG DONATION\n")
-    print ("--------------------------------------------------------------------------------------------")
-    for i in range(len(x)):
-        print("\t\t\t".join(x[i][0:4]))
-        print "\n"
-
-
-# Print thank You letter for person at index in donor's list.
-def printThankYou(index, amount):
-    print (u"Dear {name},\n\n" +
-           "Thank you for your recent donation of {donation}.\n" +
-           "The world is truly a better place because of people like you.\n\n" +
-           "Sincerely,\n" +
-           "The Charity Foundation\n").format(name=donors[index][0], donation=amount)
-
-
 # Return index of person in donor's list. Returns -1 if not in list.
-def donorIndex(person):
+def findDonor(person):
     index = -1
     for i in range(len(donors)):
         if person.lower() == donors[i][0].lower():
@@ -69,68 +39,122 @@ def donorIndex(person):
     return index
 
 
-# Add donation to person's history using index. Returns 'm' to get to main menu
-def addDonation(index):
-    userInput = ""
-    while isInt(userInput) is False and userInput != 'm':
-        userInput = raw_input("Enter the new donation amount\n" +
-                              "'m' to return to main menu\n" +
-                              "-->").decode()
-        # Back to main menu
-    if userInput.lower() == 'm':
+# Print thank You letter for person at index in donor's list.
+def printThankYou(donorIndex, amount):
+    amount = str(int(amount*100))
+    amount = (u"{dollars}.{cents}").format(dollars=amount[:len(amount)-2],
+                                           cents=amount[len(amount)-2:])
+    print (u"Dear {name},\n\n" +
+           "Thank you for your recent donation of ${donation}.\n" +
+           "The world is truly a better place because of people like you.\n\n" +
+           "Sincerely,\n" +
+           "The Charity Foundation\n").format(name=donors[donorIndex][0], donation=amount)
+
+
+# Add Donation interaction. Returns 'm' if user wants to return to main menu.
+def addDonation(donorIndex):
+    amount = ""
+    while isFloat(amount) is False and amount != 'm' or amount.startswith('-') is True:
+        amount = raw_input("Enter the new donation amount\n" +
+                           "'m' to return to main menu\n" +
+                           "-->").decode()
+    # Back to main menu
+    if amount.lower() == 'm':
         return 'm'
     else:
-        donors[index][1].append(int(userInput))
-        return userInput
+        # Add donation to person's history
+        amount = round(float(amount), 2)
+        donors[donorIndex][1].append(amount)
+        print ("----------------Thank You Letter-------------\n")
+        # Generate 'Thank You Letter'
+        printThankYou(donorIndex, amount)
+        return 'm'
+
+
+# Send Thank You interaction. Returns 'm' if user wants to return to main menu
+def sendThankYou():
+    # Accept user input: existing donor or new donor
+    person = ""
+    while person == "" or isFloat(person) is True:
+        person = raw_input("Enter the full name of an existing donor (OR) new donor\n" +
+                           "'list' to see the list of existing donors\n" +
+                           "'m' to return to main menu\n" +
+                           "-->").decode()
+        # List the donors
+        if person.lower() == "list":
+            printDonors()
+            person = ""
+    # Back to main menu
+    if person.lower() == 'm':
+        return 'm'
+    # if name does not already exist in donor list, add it
+    donorIndex = findDonor(person)
+    if donorIndex < 0:
+        donors.append([person, []])
+    # Add a donation
+    print ("----------------Add a Donation---------------\n")
+    if addDonation(donorIndex) == 'm':
+        return 'm'
+
+
+# Create & print report
+def createReport():
+    report = copy.deepcopy(donors)
+    # add data to new columns of report: total, avg, num
+    for i in range(len(report)):
+        donations = report[i][1]
+        total = 0.00
+        for j in range(len(donations)):
+            total += round(donations[j], 2)
+        num = len(donations)
+        if num == 0:
+            average = 0.00
+        else:
+            average = round(total / num, 2)
+        report[i].insert(1, average)
+        report[i].insert(1, str(num))
+        report[i].insert(1, total)
+    # sort by total donation amount
+    report = sorted(report, key=lambda donor: donor[1])
+    # add dollar signs to total & avg
+    for i in range(len(report)):
+        # ******** This works, but it's admittedly very clunky ********
+        report[i][1] = str(int(report[i][1] * 100))
+        report[i][1] = "$" + report[i][1][:len(report[i][1])-2] + "." + report[i][1][len(report[i][1])-2:]
+        report[i][3] = str(int(report[i][3] * 100))
+        report[i][3] = "$" + report[i][3][:len(report[i][3])-2] + "." + report[i][3][len(report[i][3])-2:]
+    # print report in table
+    print "NAME".rjust(25), "TOTAL DONATIONS".rjust(26),
+    print "NUM OF DONATIONS".rjust(27), "AVG DONATION".rjust(28),
+    print "\n"
+    for i in range(len(report)):
+        print report[i][0].rjust(25), report[i][1].rjust(26),
+        print report[i][2].rjust(27), report[i][3].rjust(28),
+        print "\n"
 
 
 # Main Interaction
 if __name__ == '__main__':
-    # Main Menu
-    userInputMain = ""
-    while userInputMain != 's' and userInputMain != 'c' and userInputMain != 'q':
+    # Accept user input: Main Menu
+    mainMenu = ""
+    while mainMenu != 's' and mainMenu != 'c' and mainMenu != 'q':
         print ("-----------------Main Menu-------------------\n")
-        userInputMain = raw_input("'s' to send a thank you\n" +
-                                  "'c' to create a report\n" +
-                                  "'q' to quit\n-->").decode()
-        userInputMain = userInputMain.lower()
+        mainMenu = raw_input("'s' to send a thank you\n" +
+                             "'c' to create a report (requires full-width window)\n" +
+                             "'q' to quit\n-->").decode()
         # Quit main menu
-        if userInputMain == 'q':
+        if mainMenu.lower() == 'q':
             break
-        # Enter 'Send a Thank You' Menu
-        while userInputMain == 's':
+        # Send a Thank You
+        while mainMenu.lower() == 's':
             print ("---------------Send A Thank You--------------\n")
-            userInput = ""
-            while userInput == "" or isInt(userInput) is True:
-                userInput = raw_input("Enter the full name of an existing donor (OR) new donor\n" +
-                                      "'list' to see the list of donors\n" +
-                                      "'m' to return to main menu\n" +
-                                      "-->").decode()
-                # List donors
-                if userInput.lower() == "list":
-                    printDonors()
-                    userInput = ""
-            # Back to main menu
-            if userInput.lower() == 'm':
-                userInputMain = ""
+            if sendThankYou() == 'm':
+                mainMenu = ""
                 break
-            # if name does not already exist in donor list, add it
-            if donorIndex(userInput) < 0:
-                donors.append([userInput, []])
-            print ("----------------Add a Donation---------------\n")
-            amount = addDonation(donorIndex(userInput))
-            if amount == 'm':
-                userInputMain = ""
-                break
-            else:
-                print ("----------------Thank You Letter-------------\n")
-                printThankYou(donorIndex(userInput), amount)
-                userInputMain = ""
-                break
-        # Enter Create a Report Menu
-        while userInputMain.lower() == 'c':
+        # Create a Report
+        while mainMenu.lower() == 'c':
             print ("------------------Report---------------------\n")
-            printReport()
+            createReport()
             # Back to main menu
-            userInputMain = ""
+            mainMenu = ""
             break
