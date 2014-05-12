981076eaf7afaa6c4ba7f7c6c058d8147d83a78a
Yuliya Zakharenko <rockroada@gmail.com>
2014-05-12 00:39:56 -0700

    Added test block, created test table

--- students/YuliyaZakharenko/rot13.py
+++ students/YuliyaZakharenko/rot13.py
@@ -25,18 +25,33 @@ def rot13(str):
     tran_table = string.maketrans(new_str, tran_str)
     str_encripted = str.translate(tran_table)
     return str_encripted
-    if __name__ == '__main__': 
-        #assert len(str) = len(rot13(str))
-test_table = []
-for i, val in enumerate(alphabet_lower):
-            #test_table.append.(list(enumerate(alphabet_lower, start=1))[i+13][1])
-    test_table.append([i, val])
-for i in range(len(alphabet_lower)):
-    if i <=12:
-        test_table.append([test_table[i][1], test_table[i+13][1]])
-    elif i > 12:
-        test_table.append([test_table[i][1], test_table[(i+13)%25-1][1]])
+if __name__ == '__main__':
 
-print test_table
+       #assert len(str) = len(rot13(str))   
+    alphabet_table = []
+    test_table = []
+    for i in alphabet_lower:
+            #test_table.append.(list(enumerate(alphabet_lower, start=1))[i+13][1])
+        alphabet_table.append(i)
+    for i in alphabet_upper:
+            #test_table.append.(list(enumerate(alphabet_lower, start=1))[i+13][1])
+        alphabet_table.append(i)
+
+    for i in range(len(alphabet_table)):
+        if i <=12:
+            test_table.append([alphabet_table[i], alphabet_table[i+13]])
+        elif i > 12 and i < 26:
+            test_table.append([alphabet_table[i], alphabet_table[(i+13)%25-1]])
+        if i >= 26 and i <=37:
+            test_table.append([alphabet_table[i], alphabet_table[i+13]])
+        elif i > 38:
+            test_table.append([alphabet_table[i], alphabet_table[(i+38)%50-1]])
+
+
+print 'All test passed'
+
+
+
+
 
 
