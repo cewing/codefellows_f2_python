628ce6480f23decce9e5f430ebac112a013ef092
Yuliya Zakharenko <rockroada@gmail.com>
2014-05-11 23:45:19 -0700

    Added Do you like part, fixed the deletion of the multiple occurences of the fruit

--- students/YuliyaZakharenko/list_lab.py
+++ students/YuliyaZakharenko/list_lab.py
@@ -1,3 +1,4 @@
+#! /usr/bin python
 fruits = ['Apples', 'Pears', 'Oranges', 'Peaches']
 print 'There are following fruits in the basket: ', fruits
 new_fruit = raw_input('What fruit would you like to add')
@@ -12,7 +13,7 @@ fruits.insert(0, 'Apricots')
 print fruits
 Pfruits = []
 for i in range(0, len(fruits)):
-    if fruits.startswith('P'):
+    if fruits[i].startswith('P'):
         Pfruits.append(fruits[i])
 print Pfruits   
 print fruits
@@ -26,17 +27,30 @@ else:
 print fruits
 fruits *= 2
 print fruits
-while fruit_del not in fruits:
-    fruit_del = raw_input('What fruit to delete')
-    if fruit_del in fruits:
-        for fruit_del in fruits:
-            fruits.remove(fruit_del)
-    else:
+fruit_del2 = raw_input('What fruit to delete')
+if fruit_del2 in fruits:
+    #for loop didn't work, while loop work
+    while fruit_del2 in fruits:
+        fruits.remove(fruit_del2)
+else:
+    while fruit_del2 not in fruits:
         print fruit_del, "cannot be deleted because it is not in the fruits"
 print fruits
-fruit = fruits[0].lower()
-fruit_like = raw_input("Do you like %s" %fruit)
-print fruit_like
+print len(fruits)
+# for i in range(len(fruits[:])) didn't work
+# need to figure out how to ask only once about a fruit even if there are multiple occurences in the copy of the list. 
+# then use while loop to delete all the occurences
+for i in fruits[:]:
+    fruit = i.lower()
+    fruit_like = raw_input("Do you like %s" %fruit)
+    if fruit_like in ['No', 'no']:
+        fruits.remove(i)
+    elif fruit_like in ['Yes', 'yes']:
+        pass
+    else:
+        while fruit_like not in ['No', 'no', 'Yes', 'yes']:
+            fruit_like = raw_input("Please enter Yes or No")
+print fruits
 
 
 
