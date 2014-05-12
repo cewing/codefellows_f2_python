cfa82a3a427644be9adde90e19d7aea1f0047eb9
Brent Nunn <brentnunn@yahoo.com>
2014-05-11 20:37:40 -0700

    Implemented thank you section of mailroom assignment

--- students/BrentNunn/mailroom.py
+++ students/BrentNunn/mailroom.py
@@ -11,16 +11,57 @@ donations = [[u"Fred Flinstone", 1000000],
              [u"Pink Panther", 110],
              [u"Fred Flinstone", 200000]]
 
+donors = []
+
+
+def maintain_donors_list():
+    """Maintain a list of unique donors' names"""
+
+    for donation in donations:
+        if donation[0] not in donors:
+            donors.append(donation[0])
+
+    donors.sort()
+
+
+def get_donation(donor):
+    """Get a donation from the donor"""
+
+    while True:
+        donation = raw_input(u"Enter donation amount: ")
+        if donation.isdigit():
+            donations.append([donor, int(donation)])
+            print u"Thank you {} for your generosity.".format(donor)
+            break
+        else:
+            print u"Please enter a numeric donation value"
+
+
+
 def send_thanks():
-    """Say thankyou to donors"""
+    """Say thank you to a donor"""
 
-    print u"\nThankyou for your generosity!"
+    maintain_donors_list()
 
+    while True:
+        print u"\n    Send a Thank You"
+        print u"Enter 'list' to see all past donors"
+        print u"Enter 'Q' to Quit"
+
+        name = raw_input(u"Enter a donor's full name: ")
+        if name == 'Q' or name == 'q':
+            break
+        elif name.lower() == "list":
+            print u"\nThe donors are: "
+            for donor in donors:
+                print donor
+        else:
+            print u"OK then.  Thank you {}.".format(name)
+            get_donation(name)
+            maintain_donors_list()
 
 def summarize_donor(donor):
     """Summarize donations of a donor"""
-
-    #print u"Summarizing for donor {}".format(donor)
 
     total_donations = 0
     num_donations = 0
@@ -29,37 +70,30 @@ def summarize_donor(donor):
         if donation[0] == donor:
             total_donations += donation[1]
             num_donations += 1
-
-    #print "donor_summary = {}".format([donor, total_donations, num_donations, 
-    #                                   total_donations / num_donations])
     
     return [donor, total_donations, num_donations, total_donations / num_donations]
 
 
 def total_donation(row):
+    """function used in sorting donors by total donations"""
+
     return row[1]
+
 
 def create_report():
     """Create a donors report"""
 
-    donors = []
+    maintain_donors_list()
     donors_summary = []
 
-    # Summarize donations per donor
-    for donation in donations:
-        name = donation[0]
-        if name not in donors:
-            donors.append(name)
-            donors_summary.append(summarize_donor(name))
-
+    for donor in donors:
+        donors_summary.append(summarize_donor(donor))
 
     # Sort the donors summary on total donations
     donors_summary.sort(key=total_donation)
 
-    #print u"\nHere is your report."
+    print ""
     print u"{:^59}".format("Donor's Summary")
-    #print donors_summary
-    #print ""
 
     print "{:<30}{:>12}{:>7}{:>12}".format("Name", "Total", "Count", "Average")
     for row in donors_summary:
@@ -70,10 +104,10 @@ if __name__ == '__main__':
     print u"\nWelcome to the Mail Room, Donors Application."
 
 while True:
-    print u"\n    Menu"
-    print u"T = send a Thankyou"
-    print u"R = create a Report"
-    print u"Q = Quit"
+    print u"\n    Main Menu"
+    print u"'T' = send a Thank-you"
+    print u"'R' = create a Report"
+    print u"'Q' = Quit"
 
     selection = raw_input("\nEnter your selection: ")
 
@@ -85,6 +119,4 @@ while True:
         break
     else:
         print(u"Invalid selection")
-
-
 
