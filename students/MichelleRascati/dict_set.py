bfc9a1626e39f8e6a86fcdd7a944996bad8998c4
miracode <rascatim@gmail.com>
2014-05-13 21:19:47 -0700

    dict_set through step 4

--- students/MichelleRascati/dict_set.py
+++ students/MichelleRascati/dict_set.py
@@ -1,3 +1,4 @@
+### 1 ###
 # Create Dictionary
 d = dict(name='Chris', city='Seattle', cake='Chocolate')
 # Display Dictionary
@@ -24,3 +25,41 @@ if 'Mango' in d.values():
     print "'Mango' is a value in the dictionary"
 else:
     print "'Mango' is not a value in the dictionary"
+
+### 2 ###
+# Create list of 0-15
+z_15 = range(16)
+# Create list of 0-15 in hexidecimal
+hex_z_15 = []
+for num in z_15:
+    hex_z_15.append(hex(num))
+# Zip two lists to make a dictionary
+d_hex = dict(zip(z_15, hex_z_15))
+
+### 3 ###
+# Make a dictionary with keys in d but all values 'a'
+d_a = {}
+for k in d.keys():
+    d_a[k] = 'a'
+
+### 4 ###
+# Create a set of containing 0-20 divisible by 2, 3, and 4 respectively
+s2 = set()
+s3 = set()
+s4 = set()
+for n in range(21):
+    if n % 2 == 0:
+        s2.add(n)
+    if n % 3 == 0:
+        s3.add(n)
+    if n % 4 == 0:
+        s4.add(n)
+print s2
+print s3
+print s4
+# Display if s3 is subset of s2
+print s3.issubset(s2)
+# Display if s4 is subset of s2
+print s4.issubset(s2)
+
+### 5 ###
