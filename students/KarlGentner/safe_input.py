32cfa17c0b832298b1ba24fa74bced3ebac091a5
kgentner <gentnerkarl@gmail.com>
2014-05-15 11:36:56 -0700

    removed while loop

--- students/KarlGentner/safe_input.py
+++ students/KarlGentner/safe_input.py
@@ -1,11 +1,10 @@
 # safe input exception handler
 def safe_input(message):
     """Return raw input or return None if EOFError or KeyboardInterrupt."""
-    while True:
-        try:
-            x = raw_input(message)
-            return x
-        except EOFError:
-            return None
-        except KeyboardInterrupt:
-            return None
+    try:
+        x = raw_input(message)
+        return x
+    except EOFError:
+        return None
+    except KeyboardInterrupt:
+        return None
