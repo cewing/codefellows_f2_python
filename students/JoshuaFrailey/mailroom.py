f2fe899ba189a8b75f178cd7ae5d179411bb2e9d
jefrailey <jcaetof@gmail.com>
2014-05-14 18:34:38 -0700

    Rewrite safe_input() to reduce number of lines

--- students/JoshuaFrailey/mailroom.py
+++ students/JoshuaFrailey/mailroom.py
@@ -197,10 +197,8 @@ def _print_main_menu():
 
 def safe_input(prompt):
     try:
-        input_ = unicode(raw_input(prompt))
-    except KeyboardInterrupt:
-        return None
-    except EOFError:
+        input_ = raw_input(prompt)
+    except (KeyboardInterrupt, EOFError):
         return None
     else:
         return unicode(input_)
--- students/JoshuaFrailey/safe_input.py
+++ students/JoshuaFrailey/safe_input.py
@@ -1,9 +1,7 @@
 def safe_input(prompt):
     try:
         input_ = raw_input(prompt)
-    except KeyboardInterrupt:
-        return None
-    except EOFError:
+    except (KeyboardInterrupt, EOFError):
         return None
     else:
         return unicode(input_)
