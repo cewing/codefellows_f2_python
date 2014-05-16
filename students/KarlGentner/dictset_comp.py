cdc589039866745dd4a2b96d0be3f07fc35d3661
kgentner <gentnerkarl@gmail.com>
2014-05-16 16:21:50 -0700

    added dict comps except hex series

--- students/KarlGentner/dictset_comp.py
+++ students/KarlGentner/dictset_comp.py
@@ -7,13 +7,27 @@ def displayDict(a_dict):
         print(u"{key} : {value}").format(key=key, value=value)
 
 
+# Displays the given dictionary
+def displayDictFormat(a_dict):
+    d = a_dict.copy()
+    print (u"{name} is from").format(name=d.pop('name')),
+    print (u"{city} and likes").format(city=d.pop('city')),
+    comma = False
+    for key, value in d.iteritems():
+        if comma is True:
+            print u",",
+        print (u"{value} {key}").format(key=key, value=value),
+        comma = True
+    print u"\n",
+
+
 # Displays the given set
 def displaySet(a_set):
     for x in a_set:
         print x,
 
 
-        # Displays the current set
+# Displays given list of sets
 def displayListSet(list):
     for x in range(len(list)):
         print list[x]
@@ -26,21 +40,26 @@ food_prefs = {u"name": u"Chris",
               u"fruit": u"mango",
               u"salad": u"greek",
               u"pasta": u"lasagna"}
+
 print u"\nDictionary 'food_prefs':"
-displayDict(food_prefs)
+displayDictFormat(food_prefs)
 
 
 # 1. Print the dict by passing it to a string format method, so that you get something like:
 # “Chris is from Seattle, and he likes chocolate cake, mango fruit, greek salad, and lasagna pasta”
 
 # 2.  Using a list comprehension, build a dictionary of numbers from zero to fifteen and the hexadecimal equivalent (string is fine).
-nums = range(16)
-hexnums = [hex(x) for x in nums]
-z = dict(zip(nums, hexnums))
+hexnums = [hex(x) for x in range(16)]
+z = dict(zip(range(16), hexnums))
+
 print u"\nDictionary 'z' with keys 0 to 15 and hexadecimal equivalent values:"
 displayDict(z)
 
 # 3. Do the previous entirely with a dict comprehension – should be a one-liner
+z = {i: hex(x) for x in range(16) for i in range(16)}
+
+print u"\nOne liner- Dictionary 'z' with keys 0 to 15 and hexadecimal equivalent values:"
+displayDict(z)
 
 
 # 4. Using the dictionary from item 1: Make a dictionary using the same keys but with the number of ‘a’s in each value.
@@ -52,6 +71,7 @@ displayDict(e)
 
 # 5. Create sets s2, s3 and s4 that contain numbers from zero through twenty, divisible 2, 3 and 4.
 # 5-1. Do this with one set comprehension for each set.
+# (Oops ... I already did this for the original dict set lab.)
 s2 = {i for i in range(21) if i % 2 == 0}
 s3 = {i for i in range(21) if i % 3 == 0}
 s4 = {i for i in range(21) if i % 4 == 0}
@@ -70,20 +90,23 @@ displaySet(s4)
 lsthree = [{}, {}, {}]
 for x in range(len(lsthree)):
     lsthree[x] = {i for i in range(21) if i % (x+2) == 0}
+
 print u"\n\nList with sets s2, s3, and s4:"
 displayListSet(lsthree)
 
 
 # 5c Extra credit: do it all as a one-liner by nesting a set comprehension inside a list comprehension. (OK, that may be getting carried away!)
 lsthree = [{i for i in range(21) if i % (x+2) == 0} for x in range(3)]
+
 print u"\n\nOne liner - List with sets s2, s3, and s4:"
 displayListSet(lsthree)
 
 
-# Also, how about a function that creates these set based on parameters
-def divSetGen(n=3, m=20):
-    """Return a list of 'n' sets that each contains numbers from 0 to 'm', divisible by the list index + 2 """
-    return [{i for i in range(m+1) if i % (x+2) == 0} for x in range(n)]
-lsfive = divSetGen(5)
-print u"\n\nFunction returns - List with sets s2, s3, s4, s5, and s6:"
+# Hey, how about a function that creates these sets based on parameters
+def divSetGen(n, divRange):
+    """Return a list of 'n' sets that each contains numbers from 0 to 'divRange', divisible by the list index + 2 """
+    return [{i for i in range(divRange+1) if i % (x+2) == 0} for x in range(n)]
+
+lsfive = divSetGen(5, 20)
+print u"\n\n My own add-on: Function returns - List with sets s2, s3, s4, s5, and s6:"
 displayListSet(lsfive)
