7c93648cc912692a5478e8398fcd4ce433ba70da
kgentner <gentnerkarl@gmail.com>
2014-05-15 23:41:27 -0700

    added set lab portion and cleaned up print statements a little

--- students/KarlGentner/dict_set.py
+++ students/KarlGentner/dict_set.py
@@ -2,71 +2,118 @@
 
 
 # Displays the current dictionary
-def displayDict(dictionary):
-    print u"\nCURRENT DICTIONARY:"
-    for key, value in dictionary.iteritems():
+def displayDict(a_dict):
+    for key, value in a_dict.iteritems():
         print("{key} : {value}").format(key=key, value=value)
+
+
+# Displays the current set
+def displaySet(a_set):
+    for x in a_set:
+        print x,
 
 
 # Create a dictionary containing “name”, “city”, and “cake” for “Chris” from “Seattle” who likes “Chocolate”.
 d = {u'name': u'Chris', u'city': u'Seattle', u'cake': u'Chocolate'}
+print u"\nDictionary 'd':"
 displayDict(d)
 
 
 # Delete the entry for “cake”.
-print u"\nNow let's remove the 'cake' entry"
+print u"\nDictionary 'd' with the 'cake' entry removed:"
 d.pop(u'cake')
 displayDict(d)
 
 
 # Add an entry for “fruit” with “Mango” and display the dictionary.
-print u"\nNow let's add a 'fruit' entry of 'Mango'"
+print u"\nDictionary 'd' with added 'fruit' entry of 'Mango':"
 d['fruit'] = 'Mango'
 displayDict(d)
 
 
 # Display the dictionary keys.
-print u"\nCURRENT KEYS:"
+print u"\nCurrent keys of dictionary 'd':"
 for key in d.iterkeys():
         print(key)
 
 
 # Display the dictionary values.
-print u"\nCURRENT VALUES:"
+print u"\nCurrent values of dictionary 'd':"
 for value in d.itervalues():
         print(value)
 
 
 # Display whether or not “cake” is a key in the dictionary (i.e. False) (now).
 if u'cake' in d:
-    print u"\n'cake' is a key in the current dictionary"
+    print u"\n'cake' is a key in the current dictionary."
 else:
-    print u"\n'cake' is NOT a key in the current dictionary"
+    print u"\n'cake' is NOT a key in the current dictionary."
 
 
 # Display whether or not “Mango” is a value in the dictionary.
 if u'Mango' in d.values():
-    print u"\n'Mango' is a value in the current dictionary"
+    print u"\n'Mango' is a value in the current dictionary."
 else:
-    print u"\n'Mango' is NOT a value in the current dictionary"
+    print u"\n'Mango' is NOT a value in the current dictionary."
 
 
 # Using the dict constructor and zip, build a dictionary of numbers from zero to fifteen and the hexadecimal equivalent (string is fine).
 nums = range(16)
 hexnums = [hex(x) for x in nums]
 z = dict(zip(nums, hexnums))
+print u"\nDictionary 'z' with keys 0 to 15 and hexadecimal equivalent values:"
 displayDict(z)
 
 
 # Using the dictionary from item 1: Make a dictionary using the same keys but with the number of ‘a’s in each value.
-a = {key: value.count('a') for key, value in d.iteritems()}
-displayDict(a)
+e = {key: value.count('a') for key, value in d.iteritems()}
+print u"\nDictionary 'e' with the number of a's in each value:"
+displayDict(e)
 
 
-# Create sets s2, s3 and s4 that contain numbers from zero through twenty, divisible 2, 3 and 4.
-# Display the sets.
+# Create and displaysets s2, s3 and s4 that contain numbers from zero through twenty, divisible 2, 3 and 4.
+s2 = {i for i in range(21) if i % 2 == 0}
+print u"\n\nSet s2 with numbers 0 though 20 that are divisible by 2:"
+displaySet(s2)
+s3 = {i for i in range(21) if i % 3 == 0}
+print u"\n\nSet s3 with numbers 0 though 20 that are divisible by 3:"
+displaySet(s3)
+s4 = {i for i in range(21) if i % 4 == 0}
+print u"\n\nSet s4 with numbers 0 though 20 that are divisible by 4:"
+displaySet(s4)
+
+
 # Display if s3 is a subset of s2 (False)
+if s3.issubset(s2):
+    print u"\n\ns3 is a subset of s2."
+else:
+    print u"\n\ns3 is NOT a subset of s2"
+
+
 # and if s4 is a subset of s2 (True).
+if s4.issubset(s2):
+    print u"\n\ns4 is a subset of s2."
+else:
+    print u"\n\ns4 is NOT a subset of s2"
+
+
 # Create a set with the letters in ‘Python’ and add ‘i’ to the set.
+s = set(u'Python')
+s.add(u'i')
+print u"\n\nSet 's' with letters in 'Python' and the letter 'i':"
+displaySet(s)
+
+
 # Create a frozenset with the letters in ‘marathon’
+fs = frozenset(u'marathon')
+print u"\n\nFrozen set 'fs' with letters in 'marathon':"
+displaySet(fs)
+
+
 # display the union and intersection of the two sets.
+u = s.union(fs)
+print u"\n\nThe union of set 's' and frozen set 'fs':"
+displaySet(u)
+i = fs.intersection(s)
+print u"\n\nThe intersection of set 's' and frozen set 'fs':"
+displaySet(i)
