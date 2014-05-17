b2020ccd2f316c8dd34342d679e3675b277beb27
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-17 10:02:10 -0700

    finished dictionary section of dict set lab

--- students/NathanHubbell/dict_set.py
+++ students/NathanHubbell/dict_set.py
@@ -1,3 +1,5 @@
+#Dictionaries
+
 dictionary = {"name":"Chris","city":"Seattle","cake":"Chocolate"}
 print dictionary
 dictionary.pop("cake")
@@ -7,3 +9,12 @@ print dictionary.keys()
 print dictionary.values()
 print "cake" in dictionary
 print "Mango" in dictionary
+x=range(16)
+y=(u"0",u"1",u"2",u"3",u"4",u"5",u"6",u"7",u"8",u"9",u"a",u"b",u"c",u"d",u"e",u"f")
+hexDict = dict(zip(x,y))
+for keys in hexDict:
+    hexDict[keys]=u"a"*keys
+
+
+#Sets
+s = set(range(20))
