fc8cef70acff29e124ccb92ff14a321ab4fe7c7c
Brent Nunn <brentnunn@yahoo.com>
2014-05-17 13:27:42 -0700

    Updated trigram.py so output ends with a period

--- students/BrentNunn/trigram.py
+++ students/BrentNunn/trigram.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Generate a trigram from text, and print some of the generated output"""
 
 import codecs
 import random
@@ -16,17 +17,27 @@ finally:
 
 words = text.split()
 
+#Create a dictionary of trigrams, 2 word tuples as keys, and a list
+# of words that follow those 2 words in the text
 trigrams = {}
 for i in range(len(words) - 3):
     follows = trigrams.setdefault((words[i], words[i + 1]), set())
     follows.add(words[i + 2])
 
+#Use the trigrams generated above to build a new list of words.
+#The first 2 words match the input text, for readability.
 trigram_text = [words[1], words[2]]
-for i in range(200):
+i = 0
+while True:
     choices = list(trigrams[(trigram_text[i], trigram_text[i + 1])])
     next_word = random.choice(choices)
     trigram_text.append(next_word)
+    i += 1
+    if i >= 200:
+        #After 200 words, we want the output to end on the next period
+        if next_word.count('.'):
+            break
 
 for word in trigram_text:
     print word,
-
+print ""
