13bfc3716657612668a01ef5ed8881404ca2bf6d
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-16 18:29:52 -0700

    completed the first few lines of the assignment

---
+++ students/NathanHubbell/dict_set.py
@@ -0,0 +1,9 @@
+dictionary = {"name":"Chris","city":"Seattle","cake":"Chocolate"}
+print dictionary
+dictionary.pop("cake")
+print dictionary
+dictionary["fruit"]="Mango"
+print dictionary.keys()
+print dictionary.values()
+print "cake" in dictionary
+print "Mango" in dictionary
