860556394eb091bcb7461f82db198bef4eff38f4
rivese <erives@umich.edu>
2014-05-18 16:22:22 -0700

    Added options for user input

--- students/ElizabethRives/trigrams.py
+++ students/ElizabethRives/trigrams.py
@@ -10,7 +10,7 @@ punct = list(punctuation)
 with open(filename) as open_file:
 	text = "".join(let.lower() for line in open_file for let in line if let not in punct)
 
-content = text[text.find('the golden bird') : text.find('end of project gutenbergs grimms fairy tales')]
+content = text[text.find(u'the golden bird') : text.find(u'end of project gutenbergs grimms fairy tales')]
 words = content.split()
 
 d = {}
@@ -23,18 +23,27 @@ for a, b, c in x:
 for a, b, c in x:
 	d[a, b].append(c)
 
-a, b = 'a', 'certain'
+action = raw_input(u'Please choose from the following options:\n1. Create an entirely new book\n2. Specify the number of words to generate\n')
 
-new_text = ['a', 'certain']
+if action == '1':
+	length = len(words)
+elif action == '2':
+	length = raw_input(u'How many words would you like to generate?\n')
+
+first_word = raw_input(u'Please enter the first word you would like to begin the text\n')
+second_word = raw_input(u'Please enter a second word to follow the first\n')
+
+a, b = first_word.lower(), second_word.lower()
+
+new_text = [first_word, second_word.lower()]
 
 import random
 
-for i in range(200):
+for i in range(int(length) + 1):
 	a, b = b, random.choice(d[tuple([a] + [b])])
 	new_text.append(b)
 
-print new_text
-
+print u'{}'.format(" ".join(new_text))
 
 
 
