5f3090051dad66c076bcb3c9f06e947eaf25b28c
Pat Prendergast <pat@wetdog.net>
2014-05-18 12:36:15 -0700

    Session4 file management

--- students/PatPrendergast/session4/copy_file.py
+++ students/PatPrendergast/session4/copy_file.py
@@ -10,12 +10,3 @@ def copy_to(in_file, out_file):
 
 copy_to("copy_files.txt", "copied.txt")
 
-"""
-What to do:
-open file
-read file
-write file to new name at new path.
-"""
-
-# Job:write a program which copies a file from a source, to a destination 
-# (without using shutil, or the OS copy command)
