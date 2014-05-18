ac3e1f910661235a8e7439aed7139188b8f83159
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-18 12:50:07 -0700

    FIxed mistakes in the homework.

--- students/NathanHubbell/dict_set.py
+++ students/NathanHubbell/dict_set.py
@@ -7,13 +7,14 @@ dictionary["fruit"]="Mango"
 print dictionary.keys()
 print dictionary.values()
 print "cake" in dictionary
-print "Mango" in dictionary
+print "Mango" in dictionary.values()
 x=range(16)
 y=(u"0",u"1",u"2",u"3",u"4",u"5",u"6",u"7",u"8",u"9",u"a",u"b",u"c",u"d",u"e",u"f")
 hexDict = dict(zip(x,y))
-for keys in hexDict:
-    hexDict[keys]=u"a"*keys
 
+dictDuplicate={}
+for keys in dictionary:
+    dictDuplicate[keys]=dictionary[keys].count(u"a")
 
 #Sets
 s = set(range(20))
