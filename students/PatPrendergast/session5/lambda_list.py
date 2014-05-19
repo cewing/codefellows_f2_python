652c43fb1c7ff2dac153dfbc3202e141fab72cf0
Pat Prendergast <pat@wetdog.net>
2014-05-19 14:37:24 -0700

    lambda_list.py revisions

--- students/PatPrendergast/session5/lambda_list.py
+++ students/PatPrendergast/session5/lambda_list.py
@@ -1,25 +1,27 @@
 # lambda_list.py
 
 def listmaker(n, i=1):
-    """ Creates a list of n functions to increment a chosen number (default=1) """
+    """ Creates a list of n functions to increment(i) a chosen number (default=1) """
     
-    #return [lambda e, i=1: e + i for e in range(n+1)]
+    return [lambda e, i=e: e + i for e in range(n)]
 
+'''
     fun_list = []
-    for e in range(n+1):
-        print e, i
-        fun_list.append(lambda e: e + i)
-         # increment up 1... ?
+    for e in range(n):
+        print e
+        fun_list.append(lambda e, i=e: e + i)
     return fun_list
+'''
 
-
+print '------------'
 print listmaker(5)
 fun_list = listmaker(10)
 print fun_list[3](5)
 print '------------'
-#print fun_list[4](5)
-#print fun_list[4](9,i=20) 
-
+print fun_list[4](5)
+print fun_list[4](9,i=20) 
+print '------------'
 for e in fun_list:
     print e(5)
-''' This is currently not adding e and i each time... '''
+
+
