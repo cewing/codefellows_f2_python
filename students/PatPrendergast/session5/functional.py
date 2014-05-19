4bc0a70fd83936d3cd7658076707ba69fbdb21cf
Pat Prendergast <pat@wetdog.net>
2014-05-19 15:50:02 -0700

    Functional.py exercise continued.

--- students/PatPrendergast/session5/functional.py
+++ students/PatPrendergast/session5/functional.py
@@ -1,3 +1,24 @@
 # functional.property
+import string
 
+def get_file():
+    filename = raw_input(u'What file do you wish to open?')
+    fop = open(filename, 'r')
+    contents = fop.readlines()
+    for line in contents:
+        line.strip(string.whitespace) #Not quite it.
+    return contents
 
+def write_to(word_list, outfile=None):
+    write_to = raw_input(u'''Writing to a file...
+        Do you wish to overwrite the original file? Y or N>''')
+    if write_to == u'Y' or write_to == u'y':
+        outfile.write(contents)
+    else:
+        new_file = raw_input('What file (filename) will be written to: >')
+        new_file.write(contents)
+
+contents = get_file()
+print contents
+
+''' Use map... '''
