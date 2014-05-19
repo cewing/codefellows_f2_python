351f6fa2eac791e92a741c7b1a08091fde30baed
kgentner <gentnerkarl@gmail.com>
2014-05-19 00:12:55 -0700

    added initial write set of letters

--- students/KarlGentner/mailroom.py
+++ students/KarlGentner/mailroom.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 
+import codecs
+
+
 # Original donor list with names and past donations
 donorDict = {u'Amy Akin': [200.32, 450.12, 565.24],
              u'Bob Bueller': [3234.44, 76348.03],
@@ -39,46 +42,78 @@ def printDonors(d):
     print (u"\n")
 
 
-# Print thank You letter for person at index in donor's list.
-def printThankYou(person, amount):
-    amount = str(int(amount*100))
-    amount = (u"{dollars}.{cents}").format(dollars=amount[:len(amount)-2],
-                                           cents=amount[len(amount)-2:])
+# Print the list of donors' donations
+def printDonations(person):
     print (u"\n")
-    print (u"Dear {name},\n\n" +
-           "Thank you for your recent donation of ${donation}.\n" +
-           "The world is truly a better place because of people like you.\n\n" +
-           "Sincerely,\n" +
-           "The Charity Foundation\n").format(name=person, donation=amount)
+    print (u"{name}'s donations:\n").format(name=person)
+    for donation in donorDict[person]:
+        print(u"$ " + str(donation))
+
+
+# Print thank You letter given a person and donation amount.
+# If fullSet = True, then the donation amount is a sum of all donations.
+# and the word "combined" is added to the letter
+def printThankYou(person, amount, fullSet=False):
+    if fullSet:
+        combined = u'combined '
+    else:
+        combined = u''
+    thankYouLetter = [u"\n",
+                      (u"Dear {name},\n").format(name=person),
+                      u"\n",
+                      (u"Thank you for your {combined}donation of ${donation}.\n").format(combined=combined, donation=amount),
+                      u"The world is truly a better place because of people like you.\n",
+                      u"\n",
+                      u"Sincerely,\n",
+                      u"The Charity Foundation\n"]
+    filename = (u"{name}.txt").format(name=person)
+    f = codecs.open(filename, 'w')
+    f.writelines(thankYouLetter)
+    f.close()
+    print(u"\nThank you letter saved as {name}.txt").format(name=person)
 
 
 # Add Donation interaction. Returns 'm' if user wants to return to main menu.
 def addDonation(person):
     while True:
-        amount = safe_input(u"Enter the new donation amount\n" +
-                            u"'m' to return to main menu\n" +
-                            u"-->")
-        # Back to main menu
-        if amount.lower() == u'm':
-            return u'm'
+        print(u"\nEnter a donation amount for {name}\n" +
+              u"(OR)\n" +
+              u"'list' to see {name}'s list of existing donations\n" +
+              u"'p' to return to previous menu\n").format(name=person)
+        amount = safe_input(u"-->")
+        if amount.lower() == u'p':
+            return u'p'
+        elif amount.lower() == u'list':
+            printDonations(person)
+            continue
         elif isFloat(amount) is False or amount.startswith('-'):
             print (u"\n\nThat input is not understood. Please try again.\n")
         else:
-            # Add donation to person's history
-            amount = round(float(amount), 2)
-            donorDict[person].append(amount)
-            # Generate 'Thank You Letter'
+            # If new donation, add  to person's history
+            while True:
+                selection = safe_input(u"Is this a new donation?(y/n)-->")
+                if selection.lower() == u'y':
+                    donorDict[person].append(round(float(amount), 2))
+                    break
+                elif selection.lower() == u'n':
+                    break
+                else:
+                    print (u"\n\nThat input is not understood. Please try again.\n")
+            # Generate single 'Thank You Letter' for current donation amount
             printThankYou(person, amount)
-            return u'm'
+            continue
 
 
 # Send Thank You interaction. Returns 'm' if user wants to return to main menu
 def sendThankYou():
     while True:
-        person = safe_input(u"Enter the full name of an existing donor (OR) new donor\n" +
-                            u"'list' to see the list of existing donors\n" +
-                            u"'m' to return to main menu\n" +
-                            u"-->")
+        print(u"\nTo create a thank you letter for a new donation,\n" +
+              u"enter the full name of an existing donor or new donor\n" +
+              u"(OR)\n" +
+              u"'list' to see the list of existing donors\n" +
+              u"'a' to create thank you letters for all existing donors\n" +
+              u"'m' to return to main menu\n")
+        person = safe_input(u"-->")
         if isFloat(person) is True:
             print (u"\n\nThat input is not understood. Please try again.\n")
         elif person.lower() == u"list":
@@ -87,12 +122,18 @@ def sendThankYou():
         # If 'm' then return to main menu
         elif person.lower() == u'm':
             return u'm'
+            # if 'all', print a letter for each donor's total donation
+        elif person.lower() == u'a':
+            for key in donorDict.iterkeys():
+                total = sum(donorDict[key])
+                printThankYou(key, total, True)
+            continue
         else:
             # if name does not already exist in donor list, add it
             donorDict.setdefault(person, [])
             # Add a donation
-            if addDonation(person) == u'm':
-                return u'm'
+            if addDonation(person) == u'p':
+                continue
 
 
 # Create & print report
@@ -120,9 +161,10 @@ def createReport():
 def main():
     menu = {u's': sendThankYou, u'c': createReport, u'q': exit}
     while True:
-        choice = safe_input(u"'s' to send a thank you\n" +
-                            u"'c' to create a report\n" +
-                            u"'q' to quit\n-->")
+        print(u"\n's' to send a thank you\n" +
+              u"'c' to create a report\n" +
+              u"'q' to quit\n")
+        choice = safe_input(u"-->")
         try:
             menu[choice]()
         except KeyError:
