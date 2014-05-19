93b160adceb2b6933a31ae2289aeccaad4efbd48
kgentner <gentnerkarl@gmail.com>
2014-05-19 11:23:20 -0700

    now name src and dest in terminal ie python script_name src_name dest_name

--- students/KarlGentner/copy_src2dest.py
+++ students/KarlGentner/copy_src2dest.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
+import sys
 
-src = "/home/karl/projects/cff2py/sea-c15-python/students/KarlGentner/sherlock_small.txt"
-dest = "/home/karl/projects/sher_small.txt"
+
+src = sys.argv[1]
+dest = sys.argv[2]
 
 
 def copy_src2dest(src, dest):
