c8101bc870064ce29591cf4a3206eda7464fac8a
miracode <rascatim@gmail.com>
2014-05-20 15:55:29 -0700

    close but no cigar

--- students/MichelleRascati/lambda.py
+++ students/MichelleRascati/lambda.py
@@ -1,11 +1,3 @@
-fun = lambda n: [[lambda x: x + 1] for x in range(n)]
-
-fun = lambda n, x: x + n for x in range(n)
-
-fun = [lambda n: {n: (x + n)} for x in range(n)]
-
-fun = lambda n: [lambda y: y+x for x in range(y)] 
-
 def fun(n):
     f_list = []
     for i in range(n):
@@ -13,3 +5,6 @@ def fun(n):
         f_list.append(lambda inp: inp + i)
         print f_list[i](2)
     return f_list
+
+
+fun2 = lambda n: [lambda inp: inp + i for i in range(n)]
