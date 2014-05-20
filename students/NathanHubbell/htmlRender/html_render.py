5bf9472a74ed0d2bff6c9605a638cad59761c852
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-20 12:28:33 -0700

    got part 1 working. Not a hundred percent sure about the 'ind' parameter, but everything else seems good.

---
+++ students/NathanHubbell/htmlRender/html_render.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""Python class example."""
+import codecs, os
+cwd=os.getcwd()
+
+class Element(object):
+    tag_name="html"
+    indentation="    "
+
+    def __init__(self,initContent=None):
+        self.content=[initContent]
+
+    def append(self,text):
+        self.content.append(text)
+
+    def render(self,file_out, ind=""):
+        file_out.write("<%s>\n%s%s%s\n</%s>"%(self.tag_name,self.indentation,ind,self.content,self.tag_name))
+
+#rend=Element("hello")
+#rend.append(" hello again")
+#rend.render("renderTest")
+#print rend.content
