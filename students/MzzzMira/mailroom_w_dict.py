c22138924faaa1f5a3d2c88e2e156119c0b6a986
miramzz <mira.muazzez@gmail.com>
2014-05-20 16:38:31 -0700

    update mailroom and files hw is done

--- students/MzzzMira/mailroom_w_dict.py
+++ students/MzzzMira/mailroom_w_dict.py
@@ -92,4 +92,11 @@ if __name__ == '__main__' :
 
     
 
+    #write a program which prints the full path to all files in the current directory, one per line
+    
+    
+    #write a program which copies a file from a source, to a destination (without using shutil, or the OS copy command)
+    
+
+
 
