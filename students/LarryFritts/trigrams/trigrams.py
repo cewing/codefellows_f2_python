377ba7d4f3df3d9f13c920d20d21c7ad42fbebbd
Lawrence Fritts <llfritts@gmail.com>
2014-05-20 16:23:42 -0700

    Trigrams.py works

---
+++ students/LarryFritts/trigrams/trigrams.py
@@ -0,0 +1,46 @@
+import codecs
+import random
+
+f = codecs.open("sherlock.txt", 'r')
+story = f.read().split()
+f.close()
+
+word_dict = {}
+new_story = []
+for i in range(len(story)-3):
+    word1 = story[i]
+    word2 = story[i+1]
+    word3 = story[i+2]
+    word_dict.setdefault(word1 + ' ' + word2, []).append(word3)
+
+def get_seed():
+    seed_words = word_dict.keys()
+    rnd_value = int(random.random() * len(seed_words))
+    return seed_words[rnd_value].split()
+
+seed_phrase = (get_seed())
+word0 = seed_phrase[0]
+word1 = seed_phrase[1]
+new_story.append(word0)
+new_story.append(word1)
+
+
+
+
+for i in range(200):
+    try:
+        word1 = new_story[i]
+        word2 = new_story[i+1]
+        key_word = word1 + ' ' + word2
+        word3 = word_dict[key_word][int(random.random() * len(word_dict[key_word]))]
+        new_story.append(word3)
+    except KeyError:
+        seed_phrase = (get_seed())
+        word0 = seed_phrase[0]
+        word1 = seed_phrase[1]
+        new_story.append(word0)
+        new_story.append(word1)
+        i = i + 2
+
+print " ".join(new_story)
+
