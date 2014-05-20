0d40524b15d7865c76d9b22288ea836a1a4a2f81
miracode <rascatim@gmail.com>
2014-05-20 16:07:36 -0700

    function works with kwarg

--- students/MichelleRascati/lambda.py
+++ students/MichelleRascati/lambda.py
@@ -1,10 +1,8 @@
 def fun(n):
     f_list = []
     for i in range(n):
-        print i
-        f_list.append(lambda inp: inp + i)
-        print f_list[i](2)
+        f_list.append(lambda inp, e=i: inp + e)
     return f_list
 
 
-fun2 = lambda n: [lambda inp: inp + i for i in range(n)]
+fun2 = lambda n: [lambda inp, e=i: inp + e for i in range(n)]
