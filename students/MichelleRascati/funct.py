04f3978b31bc720b28c8f4b376e1f95872e659a3
miracode <rascatim@gmail.com>
2014-05-21 11:11:21 -0700

    functional file assign complete

--- students/MichelleRascati/funct.py
+++ students/MichelleRascati/funct.py
@@ -4,17 +4,23 @@ import codecs
 def funct(f_name):
     """Remove leading and trailing whitespace from file."""
     f_read = codecs.open(f_name, 'r')
+    f_lines = f_read.readlines()
+    out_lines = map(str.strip, f_lines)
+    f_read.close()
 
     while True:
         o_write = raw_input("Create new file (c) or overwrite existing (o): ")
 
         if o_write.lower() == 'o':
-            print 'o'
-            f_write = codecs.open(f_name, 'w')
+            # f_name stays the same
             break
         elif o_write.lower() == 'c':
-            print 'c'
-            f_name_new = raw_input("What is new file name? ")
-            f_write = codecs.open(f_name_new, 'w')
+            f_name = raw_input("What is new file name? ")
             break
 
+    f_write = codecs.open(f_name, 'w')
+
+    for line in out_lines:
+        f_write.write(line + '\n')
+    print '"{}" has been written with no leading or trailing \
+whitespace.'.format(f_name)
