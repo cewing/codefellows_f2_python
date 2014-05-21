12ac1cb5f4c5817bde6e813d9be54e8c70b42a66
cewing <cris@crisewing.com>
2014-05-21 01:34:39 -0700

    pep8 fixes

--- notes/resources/UW_IntroClass/class08/code/static_method.py
+++ notes/resources/UW_IntroClass/class08/code/static_method.py
@@ -10,16 +10,14 @@ class C(object):
         print "in a_static_method"
         return a+b
     a_static_method = staticmethod(a_static_method)
-    
+
     def test(self):
-    	return self.a_static_method(2,3)
-    	
+        return self.a_static_method(2,3)
+
 if __name__ == "__main__":
 
     print C.a_static_method(3,4)
-    
+
     c = C()
-    
+
     print c.a_static_method(4,5)
-    
-    
