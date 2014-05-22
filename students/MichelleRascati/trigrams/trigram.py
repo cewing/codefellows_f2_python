580921670b4c66a3e2aa0dab4e13528178f34bc2
miracode <rascatim@gmail.com>
2014-05-21 23:26:29 -0700

    splitting up words

--- students/MichelleRascati/trigrams/trigram.py
+++ students/MichelleRascati/trigrams/trigram.py
@@ -4,5 +4,13 @@ import codecs
 
 def trigram(in_file):
     orig = codecs.open(in_file)
+    # Strip whitespace and new lines
     orig_lines = map(str.strip, orig.readlines())
-    print orig_lines
+    # Split into individual words
+    orig_words = map(str.split, orig_lines)
+    # Make 1 list
+    all_words = []
+    for line in orig_words:
+        for word in line:
+            all_words.append(word)
+    print all_words
