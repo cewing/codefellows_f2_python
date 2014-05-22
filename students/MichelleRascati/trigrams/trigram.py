b9c96bc3d1715e893c135ba8dba46714da0338c1
miracode <rascatim@gmail.com>
2014-05-21 23:55:19 -0700

    working dict

--- students/MichelleRascati/trigrams/trigram.py
+++ students/MichelleRascati/trigrams/trigram.py
@@ -13,4 +13,11 @@ def trigram(in_file):
     for line in orig_words:
         for word in line:
             all_words.append(word)
-    print all_words
+
+    tri_dict = {}
+    for i in range(len(all_words) - 2):
+        key_words = '%s %s' % (all_words[i], all_words[i + 1])
+        tri_dict.setdefault(key_words, [])
+        tri_dict[key_words].append(all_words[i + 2])
+
+    print tri_dict
