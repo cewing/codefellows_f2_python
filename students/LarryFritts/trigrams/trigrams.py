771119ece2dfc76bd3ed760ef35629590572794a
Lawrence Fritts <llfritts@gmail.com>
2014-05-22 08:47:05 -0700

    Final trigrams.py assignment

--- students/LarryFritts/trigrams/trigrams.py
+++ students/LarryFritts/trigrams/trigrams.py
@@ -1,37 +1,51 @@
+"""Trigrams Assignment"""
+
 import codecs
 import random
+import string
 
-f = codecs.open("sherlock.txt", 'r')
-story = f.read().split()
-f.close()
 
-word_dict = {}
-new_story = []
-for i in range(len(story)-3):
-    word1 = story[i]
-    word2 = story[i+1]
-    word3 = story[i+2]
-    word_dict.setdefault(word1 + ' ' + word2, []).append(word3)
-
-def get_seed():
-    seed_words = word_dict.keys()
-    rnd_value = int(random.random() * len(seed_words))
-    seed_phrase = seed_words[rnd_value].split()
+def get_seed(seed_words):
+    """Provides a key value to start a new story or recover from dead-end"""
+    seed_phrase = random.choice(seed_words).split()
     word0 = seed_phrase[0]
     word1 = seed_phrase[1]
     new_story.append(word0)
     new_story.append(word1)
 
-for i in range(200):
-    try:
-        word1 = new_story[i]
-        word2 = new_story[i+1]
-        key_word = word1 + ' ' + word2
-        word3 = word_dict[key_word][int(random.random() * len(word_dict[key_word]))]
-        new_story.append(word3)
-    except (KeyError, IndexError):
-        get_seed()
-        i = i + 2
+if __name__ == "__main__":
+    word_dict = {}
+    new_story = []
 
-print " ".join(new_story)
+    """Open text file, strip punc, make it a list"""
+    f = codecs.open("sherlock.txt", 'r')
+    story_w_punc = f.read()
+    story = ""
+    for ch in story_w_punc:
+        if ch not in string.punctuation:
+            story = story + ch
+    f.close()
+    story = story.lower().split()
+
+    """Create the trigram dictionary"""
+    for i in range(len(story)-3):
+        word1 = story[i]
+        word2 = story[i+1]
+        word3 = story[i+2]
+        word_dict.setdefault(word1 + ' ' + word2, []).append(word3)
+    seed_words = word_dict.keys()
+
+    """Build a story using the trigrams"""
+    for i in range(len(story)):
+        try:
+            word1 = new_story[i]
+            word2 = new_story[i+1]
+            key_word = word1 + ' ' + word2
+            word3 = random.choice(word_dict[key_word])
+            new_story.append(word3)
+        except (KeyError, IndexError):
+            get_seed(seed_words)
+            i = i + 2
+
+    print " ".join(new_story)
 
