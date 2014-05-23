8f61b5d46aa1221b7db7925c1d0ee828cf8927cd
miracode <rascatim@gmail.com>
2014-05-22 20:55:10 -0700

    trigram works, haven't accounted for punctuation and paragraphs

--- students/MichelleRascati/trigrams/trigram.py
+++ students/MichelleRascati/trigrams/trigram.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import codecs
+import random
 
 
-def trigram(in_file):
+def trigram(in_file, out_file):
+    """Export file by generating text from trigram algorithm from input"""
     orig = codecs.open(in_file)
     # Strip whitespace and new lines
     orig_lines = map(str.strip, orig.readlines())
@@ -14,10 +16,25 @@ def trigram(in_file):
         for word in line:
             all_words.append(word)
 
+    # Build dictionary of trigrams
     tri_dict = {}
     for i in range(len(all_words) - 2):
         key_words = '%s %s' % (all_words[i], all_words[i + 1])
         tri_dict.setdefault(key_words, [])
         tri_dict[key_words].append(all_words[i + 2])
 
-    print tri_dict
+    # Output some text
+    # Pick a random key to start
+    words = random.choice(tri_dict.keys())
+    out_lines = words
+    while True:
+        try:
+            # Get list of words corresponding to last out_words as key
+            # Then pop so we don't reuse
+            new_word = tri_dict[words].pop()
+            out_lines = '{} {}'.format(out_lines, new_word)
+            words = '{} {}'.format(words.split()[1], new_word)
+        except KeyError:
+            break
+    write_file = codecs.open(out_file, 'w')
+    write_file.write(out_lines)
