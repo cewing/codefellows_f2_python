5d76b5f806af55b1b97f91a93325a146c4aede8f
Brent Nunn <brentnunn@yahoo.com>
2014-05-24 07:32:07 -0700

    Trigram.py now accepts text file from command line

--- students/BrentNunn/trigram.py
+++ students/BrentNunn/trigram.py
@@ -3,45 +3,81 @@
 
 import codecs
 import random
+import sys
 
-try:
-    #f = codecs.open('sherlock_small.txt', 'U')
-    f = codecs.open('sherlock.txt', 'U')
-except Exception, e:
-    print 'Missing input file'
-    raise
-else:
-    text = f.read()
-finally:
-    f.close()
 
-words = text.split()
+def get_text(text_file):
+    """get a text file and split the contents into a word list"""
+    try:
+        #f = codecs.open('sherlock_small.txt', 'U')
+        #f = codecs.open('sherlock.txt', 'U')
+        f = codecs.open(text_file)
+    except Exception, e:
+        print 'Missing input file'
+        raise
+    else:
+        text = f.read()
+    finally:
+        f.close()
 
-#Create a dictionary of trigrams, 2 word tuples as keys, and a list
-# of words that follow those 2 words in the text
-trigrams = {}
-for i in range(len(words) - 3):
-    follows = trigrams.setdefault((words[i], words[i + 1]), set())
-    follows.add(words[i + 2])
+    words = text.split()
+    return words
 
-#Use the trigrams generated above to build a new list of words.
-#The first 2 words match the input text, for improved readability.
-trigram_text = [words[1], words[2]]
-i = 0
-while True:
-    choices = list(trigrams[(trigram_text[i], trigram_text[i + 1])])
-    #End if by chance the last pair of generated words were not in a trigram
-    if len(choices) == 0:
-        break
 
-    next_word = random.choice(choices)
-    trigram_text.append(next_word)
-    i += 1
-    if i >= 200:
-        #After 200 words, we want the output to end on the next period
-        if next_word.count('.'):
+def generate_trigrams(words):
+    """Generate trigrams from a list of words"""
+    #Create a dictionary of trigrams, 2 word tuples as keys, and a list
+    # of words that follow those 2 words in the text
+    trigrams = {}
+    for i in range(len(words) - 3):
+        follows = trigrams.setdefault((words[i], words[i + 1]), set())
+        follows.add(words[i + 2])
+
+    return trigrams
+
+
+def trigram_output(trigrams, first_words = None, length=200):
+    """Create text list based on a trigram dictionary."""
+    
+    #User has option to "prime" the output with initial list of words, for readability.
+    if first_words:
+        #trigram_text = [first_words[1], first_words[2]]
+        trigram_text = first_words
+    else:
+        trigram_text = list(random.choice(trigrams.keys()))
+
+    i = 0
+    while True:
+        words = list(trigrams[(trigram_text[i], trigram_text[i + 1])])
+        #End if by chance the last pair of generated words is not a trigram key
+        if len(words) == 0:
             break
 
-for word in trigram_text:
-    print word,
-print ""
+        next_word = random.choice(words)
+        trigram_text.append(next_word)
+
+        i += 1
+        if i >= length:
+            #After length reached, we want the output to end on the next period
+            if next_word.count('.'):
+                break
+
+    return trigram_text
+
+
+if __name__ == '__main__':
+    print u"\nWelcome to trigrams."
+
+    if len(sys.argv) <= 1:
+        print 'Usage: trigram.py <text_file>\n'
+        exit()
+
+    words = get_text(sys.argv[1])
+    trigrams = generate_trigrams(words)
+    starting_words = [words[0], words[1]]
+    results = trigram_output(trigrams, starting_words, length = 200)
+    for word in results:
+        print word,
+    print ""
+
+    
