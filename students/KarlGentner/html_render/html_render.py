a881eadd1271532665a0d691de7366c8578c9464
kgentner <gentnerkarl@gmail.com>
2014-05-25 16:50:49 -0700

    Steps 5, 6, 7

--- students/KarlGentner/html_render/html_render.py
+++ students/KarlGentner/html_render/html_render.py
@@ -52,7 +52,7 @@ class OneLineTag(Element):
     indent = u""
     # over-riding render method so that there are no line breaks in tag
 
-    def render(self, file_out, ind=u""):
+    def render(self, file_out, ind=u"    "):
         file_out.write(self.indent+u'<%s%s%s>' % (self.tag, self.attributes, self.link))
         for item in self.content:
             try:
@@ -67,3 +67,61 @@ class OneLineTag(Element):
 class Title(OneLineTag):
     tag = u"title"
     indent = u"    "
+
+
+class SelfClosingTag(Element):
+    def render(self, file_out, ind=u"    "):
+        file_out.write(self.indent+u'<%s%s%s' % (self.tag, self.attributes, self.link))
+        for item in self.content:
+            try:
+                # if its a Element obj, call its render method
+                item.render(file_out, self.indent + ind)
+            # if not, just write it
+            except AttributeError:
+                file_out.write(self.indent+ind+item)
+        file_out.write(u' />\n')
+
+
+class Hr(SelfClosingTag):
+    tag = u"hr"
+    indent = u"    "
+
+
+class Br(SelfClosingTag):
+    tag = u"br"
+    indent = u"    "
+
+
+class A(OneLineTag):
+    tag = u"a"
+    indent = u"    "
+
+    def __init__(self, link=None, content=None):
+        if link is not None:
+            self.link = ' href="' + link+'"'
+        Element.__init__(self, content)
+
+
+class Ul(Element):
+    tag = u"ul"
+    indent = u"    "
+
+
+class Ol(Element):
+    tag = u"ol"
+    indent = u"    "
+
+
+class Li(Element):
+    tag = u"li"
+    indent = u"        "
+
+
+class H(OneLineTag):
+    tag = u"h"
+    indent = u"    "
+
+    def __init__(self, size=None, content=None):
+        if size is not None:
+            self.tag = (u"h%s" % str(size))
+        Element.__init__(self, content)
