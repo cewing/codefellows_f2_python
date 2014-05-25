0e51489695ba4137737a684b8dbfdb69254f905f
Lawrence Fritts <llfritts@gmail.com>
2014-05-24 17:20:54 -0700

    Revise classes to better use render. Completed stage 5.

--- students/LarryFritts/htmlRender/html_render.py
+++ students/LarryFritts/htmlRender/html_render.py
@@ -1,92 +1,56 @@
-
+"""HTML Renderer Assignment"""
 
 class Element(object):
 
     tag = ''
-    indent = "    "
+    indent = ""
+    attr_str = ""
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         if content is not None:
             self.content = [content]
         else:
             self.content = []
+        if kwargs:
+            for k, v in kwargs.items():
+                self.attr_str = k + ' = ' + v
 
     def append(self, str):
-        """Do I need the try?"""
-        try:
-           self.content.append(str)
-        except TypeError:
-            pass
+        self.content.append(str)
 
     def render(self, file_out, ind=""):
-        file_out.write('<%s> \n' %self.tag)
-        for item in self.content:
-            try:
-                item.render(file_out, ind="")
-            except AttributeError:
-                file_out.write(self.indent + ind + item + '\n')
-        file_out.write('<\%s>\n' %self.tag)
-
-class Html(Element):
-    tag = 'html'
-    def render(self, file_out, ind=""):
-        file_out.write('<%s> \n' %self.tag)
+        file_out.write(self.indent + '<%s %s> \n' %(self.tag, self.attr_str))
         for item in self.content:
             try:
                 item.render(file_out, ind="    ")
             except AttributeError:
                 file_out.write(self.indent + ind + item + '\n')
-        file_out.write('<\%s>\n' %self.tag)
+        file_out.write(self.indent + '<\%s>\n' %self.tag)
+
+class Html(Element):
+    tag = 'html'
+
 
 class Body(Element):
     tag = 'body'
-    def render(self, file_out, ind=""):
-        file_out.write(ind + '<%s> \n' %self.tag)
-        for item in self.content:
-            try:
-                item.render(file_out, ind="        ")
-            except AttributeError:
-                file_out.write(self.indent + ind + item + '\n')
-        file_out.write(ind + '<\%s>\n' %self.tag)
+
 
 
 class P(Element):
     tag = 'p'
     indent = "    "
-    def render(self, file_out, ind=""):
-        file_out.write(ind + '<%s> \n' %self.tag)
-        for item in self.content:
-            try:
-                item.render(file_out, ind="    ")
-            except AttributeError:
-                file_out.write(self.indent + ind + item + '\n')
-        file_out.write(ind + '<\%s>\n' %self.tag)
+
+
 
 class Head(Element):
     tag = 'head'
-    def render(self, file_out, ind=""):
-        file_out.write(ind + '<%s> \n' %self.tag)
-        for item in self.content:
-            try:
-                item.render(file_out, ind="        ")
-            except AttributeError:
-                file_out.write(self.indent + ind + item + '\n')
-        file_out.write(ind + '<\%s>\n' %self.tag)
+
 
 class Title(Element):
     tag = 'title'
     indent = "    "
-    def render(self, file_out, ind=""):
-        file_out.write(ind + '<%s> \n' %self.tag)
-        for item in self.content:
-            try:
-                item.render(file_out, ind="        ")
-            except AttributeError:
-                file_out.write(self.indent + ind + item + '\n')
-        file_out.write(ind + '<\%s>\n' %self.tag)
 
-class Hr(Element):
-    tag = 'hr'
 
-    def render(self, file_out, ind=""):
-        file_out.write(ind + '<%s \>\n' %self.tag)
+#class Hr(Element):
+#    tag = 'hr'
+
