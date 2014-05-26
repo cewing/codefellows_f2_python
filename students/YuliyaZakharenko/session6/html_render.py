7f5718b7966522be31d45d6f9688930074550f4c
Yuliya Zakharenko <rockroada@gmail.com>
2014-05-26 16:09:07 -0700

    adding step 7 and 8

--- students/YuliyaZakharenko/session6/html_render.py
+++ students/YuliyaZakharenko/session6/html_render.py
@@ -36,6 +36,9 @@ class Element(object):
 
 class Html(Element):
     tag = 'html'
+    def render(self, file_out, ind = ""):
+        file_out.write('<!DOCTYPE html>\n')
+        Element.render(self,file_out)
 
 class Body(Element):
     tag = 'body'
@@ -44,11 +47,12 @@ class P(Element):
     tag = 'p'
 class Head(Element):
     tag = 'head'
-class Title(Element):
-    tag = 'title'
+class OneLineTag(Element):
     def render(self, file_out, ind = ""):
-
-        file_out.write('\n%s<%s>' % (ind, self.tag) )
+        attributes = ''
+        for (k,v) in self.attributes.items():
+            attributes += (' {key} = "{value}"'.format(key = k, value = v))
+        file_out.write('%s<%s%s>' % (ind, self.tag, attributes) )
         for item in self.content:
             try:
 
@@ -57,6 +61,8 @@ class Title(Element):
                 file_out.write(self.indent+ind)
                 file_out.write(item)
         file_out.write('%s</%s>' % (ind, self.tag))
+class Title(OneLineTag):
+    tag = 'title'
 class SelfClosingTag(Element):
     def render(self, file_out, ind = ""):
         attributes = ''
@@ -67,12 +73,26 @@ class Hr(SelfClosingTag):
     tag = 'hr'
 class Br(SelfClosingTag):
     tag = 'br'
-class A(Element):
+class A(OneLineTag):
     tag = 'a'
     def __init__(self, link = None, content=None):
         link = dict(href = link)
-        Element.__init__(self, content, **link)
-        
+        OneLineTag.__init__(self, content, **link)
+class Ul(Element):
+    tag = 'ul'
+class Li(Element):
+    tag = 'li'
+class H(OneLineTag):
+    def __init__(self, header_level, content=None):
+        OneLineTag.__init__(self, content)
+        #how to get the header level and pass it into the tag argument
+        self.tag = 'h%i' %header_level
+class Meta(SelfClosingTag):
+    tag = 'meta'
+
+
+
+
 
 
 
