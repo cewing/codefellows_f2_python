6074531b3635e87b6a872ea0ac84ee6eb92a56de
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-27 11:09:45 -0700

    got part 3 mainly working, with a few small glitches still

--- students/NathanHubbell/htmlRender/html_render.py
+++ students/NathanHubbell/htmlRender/html_render.py
@@ -17,19 +17,28 @@ class Html(object):
 
     def render(self,file_out, ind=""):
         file_out.write("<%s>\n%s"%(self.tag_name,self.indentation))
-        
         for element in self.content:
-            if isinstance(element,(Body,P)):
+            if isinstance(element,(Body,P,Head,Title)):
                 file_out.write("%s"%(ind))
-                element.render(file_out,"    ")
+                element.render(file_out,"    ") #consider using: element.render(file_out,ind + self.indentation)
                 file_out.write("%s"%(ind))
             else:
                 file_out.write("%s%s%s\n%s%s"%(ind,ind,element,ind,ind))
-        
         file_out.write("</%s>\n"%(self.tag_name))
 
 class Body(Html):
     tag_name="body"
 
-class P(Body):
+class P(Html):
     tag_name="p"
+
+class Head(Html):
+    tag_name="head"
+
+class OneLineTag(Html):
+   def render(self,file_out, ind=""):
+       file_out.write("<%s> %s </%s>\n"%(self.tag_name,self.content,self.tag_name))
+
+class Title(OneLineTag):
+    tag_name="title"
+
