135d433e8f41fccce042df32f739cd2640bdf654
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-27 12:23:29 -0700

    reorganized the classes to add an Element class

--- students/NathanHubbell/htmlRender/html_render.py
+++ students/NathanHubbell/htmlRender/html_render.py
@@ -3,20 +3,25 @@
 import codecs, os, inspect
 cwd=os.getcwd()
 
-class Html(object):
-    tag_name="html"
+
+
+class Element(object):
     indentation="    "
 
-    def __init__(self,initContent=None):
+    def __init__(self,initContent=None,**kwargs):
         if initContent:
             self.content=[initContent]
         else:
             self.content=[]
+        #if kwargs:
+        print kwargs
+        self.kwargs=kwargs
+
     def append(self,input):
         self.content.append(input)
 
     def render(self,file_out, ind=""):
-        file_out.write("%s<%s>\n"%(ind,self.tag_name))
+        file_out.write("%s<%s %s>\n"%(ind,self.kwargs,self.tag_name))
         for element in self.content:
             if isinstance(element,(Body,P,Head,Title)):
                 element.render(file_out,ind + self.indentation) #consider using: element.render(file_out,ind + self.indentation)
@@ -24,16 +29,20 @@ class Html(object):
                 file_out.write("%s%s%s\n"%(ind,self.indentation,element))
         file_out.write("%s</%s>\n"%(ind,self.tag_name))
 
-class Body(Html):
+class Html(Element):
+    tag_name="html"
+
+
+class Body(Element):
     tag_name="body"
 
-class P(Html):
+class P(Element):
     tag_name="p"
 
-class Head(Html):
+class Head(Element):
     tag_name="head"
 
-class OneLineTag(Html):
+class OneLineTag(Element):
     def render(self,file_out, ind=""):
         file_out.write("%s<%s> %s </%s>\n"%(ind,self.tag_name,self.content[0],self.tag_name))
 
