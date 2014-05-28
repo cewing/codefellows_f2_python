e74f63af7c7614273951288a995c804d3aa1559d
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-28 15:33:11 -0700

    finished testcircle1, moving onto number 2

--- students/NathanHubbell/Circle/circle.py
+++ students/NathanHubbell/Circle/circle.py
@@ -10,31 +10,27 @@ class Circle(object):
     def __init__(self,radius):
         self._radius = radius
         self._diameter = radius*2
+        self._area=math.pi*radius**2
     @property
     def radius(self):
         return self._radius
     @property
     def diameter(self):
         return self._diameter
+    @property
+    def area(self):
+        return self._area
     @radius.setter
     def radius(self, radius):
         self._radius = radius
         self._diameter = radius*2
+        self._area = math.pi*radius**2
     @diameter.setter
     def diameter(self, diameter):
         self._diameter = diameter
         self._radius = diameter/2
+        self._area=math.pi*(diameter/2)**2
 
 
 
 
-
-#class E(object):
-#    def __init__(self, x=5):
-#        self._x = x
-#    @property
-#    def x(self):
-#        return self._x
-#    @x.setter
-#    def x(self, value):
-#        self._x = value
