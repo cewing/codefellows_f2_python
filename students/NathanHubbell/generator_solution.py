9400fe57f7b483b038c9a118333a986e0acf7db8
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-05-29 16:34:16 -0700

    cleaned up the prime function some more

--- students/NathanHubbell/generator_solution.py
+++ students/NathanHubbell/generator_solution.py
@@ -6,7 +6,6 @@ def intsum():
         num=num+count
         count+=1
 
-
 def intsum2():
     num = 0
     count = 1
@@ -15,13 +14,11 @@ def intsum2():
         num=num+count
         count+=1
 
-
 def doubler():
     num=1
     while True:
         yield num
         num=num*2
-
 
 def fib():
     num=1
@@ -30,14 +27,12 @@ def fib():
         yield num
         num,next_num = next_num,num+next_num
 
-
 def prime():
-    num = [2]
+    num = 2
     while True:
-        not_prime=0
-        for i in num:
-            if num[-1]==i:
-                yield num[-1]
-            elif num[-1]%i==0:
+        for i in range(2,num+1):
+            if num==i:
+                yield num
+            elif num%i==0:
                 break
-        num.append(num[-1]+1)
+        num+=1
