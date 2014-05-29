7c34fbae13e53d96d240ce839cf210e72772007f
miracode <rascatim@gmail.com>
2014-05-29 12:45:37 -0700

    test_change_diamter pass

--- students/MichelleRascati/testing/circle.py
+++ students/MichelleRascati/testing/circle.py
@@ -3,9 +3,29 @@
 
 fill this in so it will pass all the tests.
 """
+
 import math
 
 
 class Circle(object):
     def __init__(self, radius):
-        self.radius = radius
+        self._radius = radius
+        self._diameter = radius * 2.0
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        self._radius = value
+        self._diameter = value * 2.0
+
+    @property
+    def diameter(self):
+        return self._diameter
+
+    @diameter.setter
+    def diameter(self, value):
+        self._diameter = value
+        self._radius = value / 2.0
