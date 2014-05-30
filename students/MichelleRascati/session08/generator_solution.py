7b2ef86a50ea212f4375fccc616b856ce182a725
miracode <rascatim@gmail.com>
2014-05-29 19:17:44 -0700

    test_initsum2 pass (not sure what diff should be

--- students/MichelleRascati/session08/generator_solution.py
+++ students/MichelleRascati/session08/generator_solution.py
@@ -3,7 +3,17 @@
 Generators
 """
 
+
 def intsum():
+    i = 0
+    n = 0
+    while True:
+        yield i
+        n += 1
+        i = i + n
+
+
+def intsum2():
     i = 0
     n = 0
     while True:
